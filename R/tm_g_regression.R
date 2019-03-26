128347a1d8d8322bbcdcfaf5f77b7e8ce31f35d9
wolfs25 <sebastian.wolf.sw1@roche.com>
2019-03-26 23:55:40 +0000

    Changed tm_g_regression #23 to use code chunks and data merging

--- R/tm_g_regression.R
+++ R/tm_g_regression.R
@@ -15,12 +15,12 @@
 #' @inheritParams teal::standard_layout
 #'
 #' @examples
-#'
+#' 
 #' library(random.cdisc.data)
 #' asl <- radsl(N = 600)
-#'
+#' 
 #' adte <- radtte(asl, event.descr = c("STUDYID", "USUBJID", "PARAMCD"))
-#'
+#' 
 #' adte_filters <- filter_spec(
 #'   vars = c("PARAMCD"), #'  only key variables are allowed
 #'   sep = " - ",
@@ -29,8 +29,8 @@
 #'   multiple = TRUE, #'  if multiple, then a spread is needed
 #'   label = "Choose endpoint"
 #' )
-#'
-#'
+#' 
+#' 
 #' adte_extracted_regressor <- data_extract_spec(
 #'   dataname = "ADTE",
 #'   filter = adte_filters,
@@ -42,7 +42,7 @@
 #'     label = "Column" #'  Label the column select dropdown (optional)
 #'   )
 #' )
-#'
+#' 
 #' adte_extracted_response <- data_extract_spec(
 #'   dataname = "ADTE",
 #'   filter = adte_filters,
@@ -54,7 +54,7 @@
 #'     label = "" #'  Label the column select dropdown (optional)
 #'   )
 #' )
-#'
+#' 
 #' asl_extracted <- data_extract_spec(
 #'   dataname = "ASL",
 #'   columns = columns_spec(
@@ -64,7 +64,7 @@
 #'     fixed = FALSE
 #'   )
 #' )
-#'
+#' 
 #' app <- teal::init(
 #'   data = cdisc_data(
 #'     ASL = data_for_teal(
@@ -84,8 +84,8 @@
 #'       dataname = c("ASL", "ADTE"),
 #'       response = list(adte_extracted_response),
 #'       regressor = list(
-#'         adte_extracted_regressor,
-#'         asl_extracted
+#'         asl_extracted,
+#'         adte_extracted_regressor
 #'       )
 #'     )
 #'   )
@@ -106,6 +106,9 @@ tm_g_regression <- function(
   stopifnot(!is.null(dataname))
   stopifnot(dataname != "")
 
+  # No check necessary for regressor and response, as checked in data_extract_input
+
+  # Send ui args
   args <- as.list(environment())
 
   teal::module(
@@ -113,7 +116,7 @@ tm_g_regression <- function(
     server = srv_g_regression,
     ui = ui_g_regression,
     ui_args = args,
-    server_args = list(regressor = regressor, response = response),
+    server_args = list(regressor = regressor, response = response, dataname = dataname),
     filters = "all"
   )
 }
@@ -125,7 +128,6 @@ ui_g_regression <- function(id, ...) {
   arguments <- list(...)
 
   ns <- NS(id)
-
   standard_layout(
     output = teal.devel::white_small_well(
       tags$div(
@@ -135,7 +137,7 @@ ui_g_regression <- function(id, ...) {
       )
     ),
     encoding = div(
-      helpText("Datasets", paste(unlist(arguments$dataname))),
+      helpText("Datasets: ", arguments$dataname %>% lapply(., tags$code)),
       data_extract_input(
         id = ns("regressor"),
         label = "Regressor Variable",
@@ -158,19 +160,22 @@ ui_g_regression <- function(id, ...) {
       ),
       # This shall be wrapped in a teal::plot
       plot_height_input(id = ns("myplot"), value = arguments$plot_height)
-    )
+    ),
+    pre_output = arguments$pre_output,
+    post_output = arguments$post_output,
+    forms = actionButton(ns("show_rcode"), "Show R Code", width = "100%")
   )
 }
 
 #' @importFrom graphics plot abline
 #' @importFrom methods is
-srv_g_regression <- function(input, output, session, datasets, response, regressor) {
+srv_g_regression <- function(input, output, session, datasets, dataname, response, regressor) {
   stopifnot(is.list(response))
   stopifnot(is.list(regressor))
 
-  # data_extract_module, "response",
-  # dataname AND filtering (yes/no) AND Names(Filtering-selected) AND Names(Columns-Selected)
+  use_chunks(session)
 
+  # Data Extraction
   regressor_data <- callModule(data_extract_module,
     id = "regressor",
     datasets = datasets,
@@ -182,67 +187,113 @@ srv_g_regression <- function(input, output, session, datasets, response, regress
     data_extract_spec = response
   )
 
-  fit_cl <- reactive({
-    call("lm",
-      formula = as.formula(
-        paste(get_dataset_prefixed_col_names(response_data()),
-          paste(get_dataset_prefixed_col_names(regressor_data()),
-            collapse = " + "
-          ),
-          sep = " ~ "
-        )
-      ),
-      data = as.name("merged_dataset")
-    )
+  merged_dataset <- reactive({
+    set_chunk("formula", rlang::expr(
+      paste(response_var,
+        paste(regressor_var,
+          collapse = " + "
+        ),
+        sep = " ~ "
+      )
+    ), vars = list(
+      dataname = "",
+      response_var = get_dataset_prefixed_col_names(response_data()),
+      regressor_var = get_dataset_prefixed_col_names(regressor_data())
+    ))
+    merge_datasets(regressor_data(), response_data())
   })
 
-  fit <- reactive({
-    merged_dataset <- merge_datasets(regressor_data(), response_data())
-    fit_cl <- fit_cl()
+  set_lm <- reactive({
+    validate_has_data(merged_dataset(), 10)
 
-    validate(
-      need(nrow(merged_dataset) >= 10, paste("need at lease 10 observations, currently have only",
-              nrow(merged_dataset)))
+    set_chunk("lm",
+      # Please note rlang::expr ignores the left hand side of the "<-"
+      rlang::expr(
+        fit <- lm(as.formula(form), data = dataset)
+      ),
+      vars = list(
+        dataset = merged_dataset(),
+        dataname = "merged_dataset",
+        form = eval_chunk("formula")
+      )
     )
-
-    attr(fit_cl[[2]], ".Environment") <- environment()
-
-    fit <- eval(fit_cl)
-    fit
   })
 
   output$plot <- renderPlot({
-    fit <- fit()
-    plot_type <- input$plot_type
-    if (plot_type == "Response vs Regressor") {
-      if (ncol(fit$model) > 1) {
-        plot(fit$model[, 2:1])
-      }else{
-        # Relabel the data for X vs X plot
-        plot_data <- data.frame(fit$model[, 1], fit$model[, 1])
-        names(plot_data) <- rep(names(fit$model), 2)
-        plot(plot_data)
-      }
-      abline(fit)
-    } else {
-      i <- which(plot_type == c(
-        "Residuals vs Fitted",
-        "Normal Q-Q", "Scale-Location", "Cook's distance", "Residuals vs Leverage",
-        "Cook's dist vs Leverage h[ii]/(1 - h[ii]"
-      ))
+    set_lm() # Re-act to changes in linear model
 
-      plot(fit, which = i, id.n = NULL)
-    }
+    set_chunk("plot",
+      plot_expression(eval_chunk("lm"), input$plot_type),
+      vars = list(
+        dataset = eval_chunk("lm"),
+        dataname = get_lhs_chunk("lm"),
+        i = which(input$plot_type == c(
+          "Residuals vs Fitted",
+          "Normal Q-Q", "Scale-Location", "Cook's distance", "Residuals vs Leverage",
+          "Cook's dist vs Leverage h[ii]/(1 - h[ii]"
+        ))
+      )
+    )
+
+    eval_chunk("plot")
   })
 
   # Insert the plot into a plot_height module from teal.devel
   callModule(plot_with_height, id = "myplot", plot_height = reactive(input$myplot), plot_id = session$ns("plot"))
 
   output$text <- renderPrint({
-    fit <- fit()
+    set_lm()
+    set_chunk("summary",
+      my_expr <- rlang::expr(
+        summary(dataset)
+      ),
+      vars = list(
+        dataset = eval_chunk("lm"),
+        dataname = get_lhs_chunk("lm")
+      )
+    )
 
-    validate(need(methods::is(fit, "lm"), "there seem to problems fitting the model"))
-
-    summary(fit)
+    summary <- eval_chunk("summary")
+    summary$call <- paste0("summary(lm(as.formula(", eval_chunk("formula"), "), data = dataset))")
+    return(summary)
   })
+
+  observeEvent(input$show_rcode, {
+    # Just show R Code with valid data
+    validate_has_data(merged_dataset(), 10)
+
+    teal.devel::show_rcode_modal(
+      title = "R Code for a Regression Plot",
+      rcode = get_rcode(
+        datasets = datasets,
+        dataname = dataname,
+        merged_dataname = "merged_dataset",
+        merged_datasets = list(response_data(), regressor_data()),
+        title = paste("RegressionPlot of ", eval_chunk("formula")),
+        description = "",
+        libraries = c(),
+        git_pkgs = list(roche = c("NEST/teal", "NEST/teal.devel", "NEST/teal.modules.general")),
+        selected_chunk_ids = c("lm", "summary", "plot")
+      )
+    )
+  })
+}
+
+plot_expression <- function(fit, plot_type) {
+  if (plot_type == "Response vs Regressor") {
+    if (ncol(fit$model) > 1) {
+      validate(need(dim(fit$model)[2] < 3, "Response vs Regressor is not provided for >2 Regressors"))
+      rlang::expr(plot(dataset$model[, 2:1]))
+    } else {
+      rlang::expr({
+        plot_data <- data.frame(dataset$model[, 1], dataset$model[, 1])
+        # Relabel the data for X vs X plot
+        names(plot_data) <- rep(names(dataset$model), 2)
+        plot(plot_data)
+        abline(dataset)
+      })
+    }
+  } else {
+    rlang::expr(plot(dataset, which = i, id.n = NULL))
+  }
 }
