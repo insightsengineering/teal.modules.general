91ef5c04761d32c97e9804f280f0b586afd24db0
slamao <ondrej.slama@roche.com>
2019-05-02 16:19:37 +0200

    scatterplotmatrix rewritten to lattice, added show R code button

--- R/tm_g_scatterplotmatrix.R
+++ R/tm_g_scatterplotmatrix.R
@@ -12,7 +12,7 @@
 #' @export
 #'
 #' @examples
-#' # libraries: shiny, magrittr
+#' # libraries: shiny, magrittr, teal.devel
 #' asl <- random.cdisc.data::radsl(N = 600)
 #' adte <- random.cdisc.data::radtte(asl, event.descr = c("STUDYID", "USUBJID", "PARAMCD"))
 #' teal.devel::keys(adte) <- c("STUDYID", "USUBJID", "PARAMCD")
@@ -31,8 +31,8 @@
 #'   dataname = "ADTE",
 #'   filter = adte_filters,
 #'   columns = teal.devel::columns_spec(
-#'     choices = c("*no columns selected*" = "", colnames(adte)),
-#'     selected = "",
+#'     choices = colnames(adte),
+#'     selected = colnames(adte)[1:2],
 #'     multiple = TRUE,
 #'     fixed = FALSE, #'  Whether the user can select the item
 #'     label = "" #'  Label the column select dropdown (optional)
@@ -84,43 +84,40 @@ ui_g_scatterplotmatrix <- function(id, ...) {
 
   ns <- NS(id)
   teal::standard_layout(
-    output = teal.devel::white_small_well(
+    output = white_small_well(
       tags$div(
-        tags$div(
-          teal.devel::white_small_well(
-            teal.devel::plot_height_output(ns("myplot")))
-        )
-      )
+        plot_height_output(ns("myplot")))
     ),
     encoding = div(
       helpText("Datasets: ", args$dataname %>% lapply(., tags$code)),
-      teal.devel::data_extract_input(
+      data_extract_input(
         id = ns("select_col"),
         label = "Selected columns",
         data_extract_spec = args$select_col
       ),
-      sliderInput(ns("bins"), "Number of bins in histograms", min = 1, max = 100,
-                  value = 30, round = TRUE, step = 3),
-      teal.devel::plot_height_input(id = ns("myplot"), value = args$plot_height)
+      sliderInput(ns("alpha"), "Transparency",
+                  min = 0, max = 1, step = .05, value = .5, ticks = FALSE),
+      sliderInput(ns("cex"), "Point Size",
+                  min = 0.2, max = 3, step = .05, value = .65, ticks = FALSE),
+      plot_height_input(id = ns("myplot"), value = args$plot_height)
     ),
     pre_output = args$pre_output,
-    post_output = args$post_output
+    post_output = args$post_output,
+    forms = actionButton(ns("show_rcode"), "Show R Code", width = "100%")
   )
 }
 
-# TO DO: import GGally ?? (only requireNamespace should be enough -> discuss)
-# TO DO: no columns selected? keep it as an option?
-# TO DO: check for cardinality - different column names in validate(need()) than in selectInput: ADTE.USUBJID vs USUBJID
+
 srv_g_scatterplotmatrix <- function(input, output, session, datasets, dataname, select_col) {
 
   # checks
   stopifnot(is.list(select_col))
 
   # setup to use chunks
-  teal.devel::use_chunks(session)
+  use_chunks(session)
 
   # data extraction
-  col_extract <- callModule(teal.devel::data_extract_module,
+  col_extract <- callModule(data_extract_module,
                             id = "select_col",
                             datasets = datasets,
                             data_extract_spec = select_col
@@ -128,56 +125,62 @@ srv_g_scatterplotmatrix <- function(input, output, session, datasets, dataname, 
 
   # reactive handling of user inputs to produce a scatterplotmatrix
   reprod_plot <- reactive({
-    # get number of bins for histograms
-    bins <- input$bins
+    # get inputs
+    alpha <- input$alpha
+    cex <- input$cex
 
     # get selected columns
-    cols <- teal.devel::get_dataset_prefixed_col_names(col_extract())
+    cols <- get_dataset_prefixed_col_names(col_extract())
 
     # merge datasets
-    merged_ds <- teal.devel::merge_datasets(list(col_extract()))
+    merged_ds <- merge_datasets(list(col_extract()))
 
     # check columns selected
     validate(need(cols,
                   "Please select columns first."))
 
+    # lattice need at least 2 columns for the plot
+    validate(need(length(cols) >= 2,
+                  "Please select at least two columns."))
+
+
     # check that data are available
     validate(need(nrow(merged_ds) > 0,
                   "There are zero observations in the (filtered) dataset."))
 
-    # check for high cardinality (factor variables with high number of levels take long time to be plotted ...)
-    len_levels <- vapply(merged_ds[, cols, drop = F], function(x) {
-      if (is.character(x)) {length(levels(as.factor(x)))} else {0}
-    }, 1)
-    ggpairs_thresh <- as.list(args(GGally::ggpairs))[["cardinality_threshold"]]
-    invalid_cols <- names(len_levels)[len_levels > ggpairs_thresh]
-    validate(need(identical(invalid_cols, character(0)),
-                  paste0("There is/are column(-s) with high number of levels (> ",
-                         ggpairs_thresh,
-                         "), please remove it/them: ",
-                         paste(invalid_cols, collapse = ", "))))
+    # check proper input values
+    validate(need(cex, "Need a proper cex value."))
+
+    # check proper input values
+    validate(need(alpha, "Need a proper alpha value."))
 
     # reset chunks on every user-input change
-    teal.devel::renew_chunk_environment(envir = environment())
-    teal.devel::renew_chunks()
+    renew_chunk_environment(envir = environment())
+    renew_chunks()
 
-    # set up expression chunk
-    teal.devel::set_chunk(
+    # set up expression chunk - lattice graph
+    set_chunk(
       expression = quote(
-        expr = GGally::ggpairs(
-          data = merged_ds,
-          columns = .cols,
-          diag = list(continuous = GGally::wrap(funcVal = "barDiag", bins = .bins)))) %>%
-        methods::substituteDirect(list(.cols = cols, .bins = bins))
+        merged_ds <- dplyr::mutate_if(merged_ds, is.character, as.factor)
+      )
     )
 
+    # set up expression chunk - lattice graph
+    plt %<chunk%
+        methods::substituteDirect(
+          object = quote(
+            lattice::splom(merged_ds[, .cols], pch = 16, alpha = .alpha, cex = .cex)
+          ),
+          frame = list(.cols = cols, .alpha = alpha, .cex = cex)
+        )
+
     # evaluate chunk
-    teal.devel::eval_remaining()
+    eval_remaining()
 
   })
 
   # set plot output
-  callModule(teal.devel::plot_with_height,
+  callModule(plot_with_height,
              id = "myplot",
              plot_height = reactive(input$myplot),
              plot_id = session$ns("plot"))
@@ -185,6 +188,29 @@ srv_g_scatterplotmatrix <- function(input, output, session, datasets, dataname, 
   # plot
   output$plot <- renderPlot({
     reprod_plot()
+  })
+
+  # show r code
+  observeEvent(input$show_rcode, {
+
+    title <- paste0("Scatterplotmatrix of ",
+                    paste(get_dataset_prefixed_col_names(col_extract()),
+                          collapse = ", "))
+
+    show_rcode_modal(
+      title = "R Code for a Scatterplotmatrix",
+      rcode = get_rcode(
+        datasets = datasets,
+        dataname = dataname,
+        merged_dataname = "merged_ds",
+        merged_datasets = list(col_extract()),
+        title = title,
+        description = "",
+        libraries = c("random.cdisc.data"),
+        git_pkgs = list(roche = c("NEST/teal", "NEST/random.cdisc.data", "NEST/teal.devel",
+                                  "NEST/teal.modules.general"))
+      )
+    )
   })
 
 }
