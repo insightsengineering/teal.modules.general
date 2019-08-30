a02dc9f7b244111857e8dd6373b33dce38c33f16
Mordig, Maximilian Oliver {FPDC~Kaiseraugst} <maximilian_oliver.mordig@roche.com>
2019-08-30 18:01:40 +0200

    fixed merge conflicts and fixed small things, now refactoring (#186)
    * fixed merge conflicts and fixed small things, now refactoring
    
    * fix example
    
    * rewritten and refactored
    
    * fixed
    
    * fixed
    
    * fixed
    
    * fixed
    
    * fix examples

--- R/tm_g_scatterplotmatrix.R
+++ R/tm_g_scatterplotmatrix.R
@@ -1,5 +1,10 @@
 #' Create a scatterplot matrix
 #'
+#' The module allows to add and remove dataset selectors (data extract inputs) and it will create
+#' the scatterplot matrix for a combination of the selected columns in the merge of all such datasets.
+#' The available datasets to choose from for each dataset selector is the same and
+#' determined by the argument \code{selected}.
+#'
 #' @inheritParams teal::module
 #' @inheritParams teal.devel::standard_layout
 #' @param selected (\code{data_extract_spec} or \code{list} of multiple \code{data_extract_spec})
@@ -9,6 +14,7 @@
 #' @inheritParams teal::module
 #' @inheritParams teal.devel::standard_layout
 #' @export
+#' @importFrom stats na.omit
 #' @examples
 #'
 #' # datasets: single wide dataset
@@ -46,32 +52,18 @@
 #' # datasets: different wide
 #' # Scatterplot matrix with AGE, ACTARM, SEX, COUNTRY and STRATA2
 #' library(random.cdisc.data)
-#' library(dplyr)
 #'
 #' ADSL <- cadsl
-#' ADSL <- mutate_at(ADSL,
-#'   .vars = vars(c("ARM", "ACTARM", "ACTARMCD", "SEX", "STRATA1", "STRATA2")),
-#'   .funs = list(~as.factor(.))
-#' ) %>% select(
-#'   "ARM", "ACTARM", "ACTARMCD",
-#'   "SEX", "STRATA1", "AGE", "USUBJID", "STUDYID", "STRATA2"
-#' )
-#'
-#' ADSL_2 <- mutate_at(cadsl,
-#'   .vars = vars(c("ARM", "ACTARM", "ACTARMCD", "SEX", "STRATA1", "STRATA2")),
-#'   .funs = list(~as.factor(.))
-#' ) %>% select("ACTARM", "AGE", "STRATA2", "COUNTRY", "USUBJID", "STUDYID")
+#' ADSL_2 <- cadsl
+#' ADSL_3 <- cadsl
 #'
 #' app <- init(
 #'   data = cdisc_data(
 #'     cdisc_dataset("ADSL", ADSL),
 #'     dataset("ADSL_2", ADSL_2, keys = get_cdisc_keys("ADSL")),
-#'     code = 'ADSL <- cadsl
-#' ADSL_2 <- mutate_at(cadsl,
-#' .vars = vars(c("ARM", "ACTARM", "ACTARMCD", "SEX", "STRATA1", "STRATA2")),
-#' .funs = list(~as.factor(.))) %>%
-#' select("ACTARM", "AGE", "STRATA2", "COUNTRY", "USUBJID", "STUDYID")',
-#'     check = FALSE #TODO
+#'     dataset("ADSL_3", ADSL_2, keys = get_cdisc_keys("ADSL")),
+#'     code = 'ADSL <- cadsl; ADSL_2 <- cadsl; ADSL_3 <- cadsl',
+#'     check = TRUE
 #'   ),
 #'   modules = root_modules(
 #'     tm_g_scatterplotmatrix(
@@ -93,6 +85,16 @@
 #'             label = "Select variables:",
 #'             choices = colnames(ADSL_2),
 #'             selected = c("COUNTRY", "ACTARM", "STRATA2"),
+#'             multiple = TRUE,
+#'             fixed = FALSE
+#'           )
+#'         ),
+#'         data_extract_spec(
+#'           dataname = "ADSL_3",
+#'           select = select_spec(
+#'             label = "Select variables:",
+#'             choices = colnames(ADSL_3),
+#'             selected = c("AGE", "ACTARM", "SEX"),
 #'             multiple = TRUE,
 #'             fixed = FALSE
 #'           )
@@ -211,38 +213,39 @@
 #'
 #' ADSL <- cadsl
 #' ADLB <- cadlb
+#' ADRS <- cadrs
 #'
 #' app <- init(
 #'   data = cdisc_data(
 #'     cdisc_dataset("ADSL", ADSL),
-#'     cdisc_dataset("ADLB", ADLB),
-#'     code = "ADSL <- cadsl; ADLB <- cadlb",
+#'     cdisc_dataset("ADRS", ADRS),
+#'     code = "ADSL <- cadsl; ADRS <- cadrs",
 #'     check = TRUE
 #'   ),
 #'   modules = root_modules(
 #'     tm_g_scatterplotmatrix(
 #'       label = "Scatterplot matrix on two long datasets with subsets",
 #'       selected = data_extract_spec(
-#'         dataname = "ADLB",
+#'         dataname = "ADRS",
 #'         filter = list(
 #'           filter_spec(
 #'             vars = "PARAMCD",
-#'             choices = levels(ADLB$PARAMCD),
-#'             selected = levels(ADLB$PARAMCD)[1],
+#'             choices = levels(ADRS$PARAMCD),
+#'             selected = levels(ADRS$PARAMCD)[1],
 #'             multiple = FALSE,
 #'             label = "Select lab:"
 #'           ),
 #'           filter_spec(
 #'             vars = "AVISIT",
-#'             choices = levels(ADLB$AVISIT),
-#'             selected = levels(ADLB$AVISIT)[1],
+#'             choices = levels(ADRS$AVISIT),
+#'             selected = levels(ADRS$AVISIT)[1],
 #'             multiple = FALSE,
 #'             label = "Select visit:"
 #'           )
 #'         ),
 #'         select = select_spec(
-#'           choices = c("AVAL", "CHNG", "BMRKR1", "BMRKR2"),
-#'           selected = c("AVAL", "CHNG"),
+#'           choices = c("AVAL", "BMRKR1", "BMRKR2", "AGE"),
+#'           selected = c("AVAL", "AGE"),
 #'           multiple = TRUE,
 #'           fixed = FALSE,
 #'           label = "Select variables:"
@@ -273,9 +276,7 @@ tm_g_scatterplotmatrix <- function(label = "Scatterplot matrix",
 
   module(
     label = label,
-    server = function(input, output, session, datasets, ...) {
-      return(NULL)
-    },
+    server = srv_g_scatterplotmatrix,
     ui = ui_g_scatterplotmatrix,
     ui_args = args,
     server_args = list(selected = selected),
@@ -290,23 +291,20 @@ ui_g_scatterplotmatrix <- function(id, ...) {
   ns <- NS(id)
   standard_layout(
     output = white_small_well(
-      tags$div(
-        plot_height_output(ns("myplot"))
-      )
+      plot_height_output(ns("myplot"))
     ),
     encoding = div(
       tags$label("Encodings", class = "text-primary"),
-      datanames_input(args["selected"]),
-      data_extract_input(
-        id = ns("selected"),
-        label = "Selected variables",
-        data_extract_spec = args$selected
+      datanames_input(args$selected),
+      actionLink(ns("add_selector"), "Add Selector"),
+      hr(),
+      sliderInput(
+        ns("alpha"), "Opacity:", min = 0, max = 1,
+        step = .05, value = .5, ticks = FALSE
       ),
-      sliderInput(ns("alpha"), "Opacity:",
-        min = 0, max = 1, step = .05, value = .5, ticks = FALSE
-      ),
-      sliderInput(ns("cex"), "Points size:",
-        min = 0.2, max = 3, step = .05, value = .65, ticks = FALSE
+      sliderInput(
+        ns("cex"), "Points size:", min = 0.2, max = 3,
+        step = .05, value = .65, ticks = FALSE
       ),
       plot_height_input(id = ns("myplot"), value = args$plot_height)
     ),
@@ -314,6 +312,27 @@ ui_g_scatterplotmatrix <- function(id, ...) {
     post_output = args$post_output,
     forms = actionButton(ns("show_rcode"), "Show R code", width = "100%")
   )
+}
+
+#' HTML id of the UI element that contains the extract input
+#' @param idx index or identifier of the data extract UI that contains the input element
+#' @return the html id of the element given the idx
+extract_ui_id <- function(idx) {
+  paste0("extract_ui_", idx)
+}
+
+#' HTML id of the extract input contained within another UI
+#' @param idx index or identifier of the data extract input element
+#' @return the html id of the element given the idx
+extract_input_id <- function(idx) {
+  paste0("extract_input_", idx)
+}
+
+#' HTML id of the element to remove the whole extract UI
+#' @param idx index or identifier of the button to remove the data extract ui
+#' @return the html id of the element given the idx
+remove_extract_ui_id <- function(idx) {
+  paste0("remove_extract_ui_", idx)
 }
 
 
@@ -325,93 +344,108 @@ srv_g_scatterplotmatrix <- function(input,
                                     session,
                                     datasets,
                                     selected) {
-  dataname <- get_extract_datanames(list(selected))
 
-  # setup to use chunks
-  init_chunks()
+  init_chunks(session)
 
-  # data extraction
-  col_extract <- callModule(
-    data_extract_module,
-    id = "selected",
-    datasets = datasets,
-    data_extract_spec = selected
-  )
+  extract_ui_indices <- reactiveVal(value = NULL)
 
-  # set plot output
+  # add_ui is called whenever a new ui element should be added, it contains the id of the next ui element to be added
+  add_ui <- reactive({
+    # index of new data extract UI
+    idx <- max(extract_ui_indices(), 0) + 1 # also works when empty
+    insertUI(
+      selector = paste0("#", session$ns("add_selector")),
+      where = "beforeBegin",
+      ui = div(
+        id = extract_ui_id(idx),
+        data_extract_input(
+          id = session$ns(paste0("extract_input_", idx)),
+          label = paste0("Selector (", idx, ")"),
+          data_extract_spec = selected
+        ),
+        div(
+          actionLink(session$ns(remove_extract_ui_id(idx)), "Remove"),
+          align = "right"
+        )
+      )
+    )
+    extract_ui_indices(c(extract_ui_indices(), idx))
+
+    # bind remove event
+    observeEvent(
+      input[[remove_extract_ui_id(idx)]],
+      {
+        # executed within an isolate block
+        # need at least one data extract to select from, validate(need) not working here without output element
+        if (length(extract_ui_indices()) <= 1) {
+          # todo: somehow show in interface
+          print("need at least one data extract to select from")
+        }
+        req(length(extract_ui_indices()) > 1)
+        removeUI(selector = paste0("#", extract_ui_id(idx)))
+        extract_ui_indices(extract_ui_indices()[extract_ui_indices() != idx])
+      }
+    )
+  })
+
+  observeEvent(input$add_selector, add_ui())
+  observeEvent(extract_ui_indices(), ignoreNULL = FALSE, {
+    # ignoreNULL = FALSE and is.null(extract_ui_indices() --> only run on startup
+    req(is.null(extract_ui_indices()))
+    # add two data extracts initially
+    add_ui()
+    add_ui()
+  })
+
+  merged_data <- reactive({
+    validate(need(length(extract_ui_indices()) > 0, "Need at least 1 input."))
+    data_merge_module(
+      datasets = datasets,
+      data_extract = replicate(length(extract_ui_indices()), selected, simplify = FALSE),
+      input_id = vapply(extract_ui_indices(), function(idx) extract_input_id(idx), character(1))
+    )()
+  })
+
+  # Insert the plot into a plot_height module
   callModule(
     plot_with_height,
     id = "myplot",
     plot_height = reactive(input$myplot),
-    plot_id = session$ns("plot"))
+    plot_id = session$ns("plot")
+  )
 
   # plot
   output$plot <- renderPlot({
-    # get inputs
-    alpha <- input$alpha
-    cex <- input$cex
-
-    # get selected columns
-    cols <- get_dataset_prefixed_col_names(col_extract())
-
-    # merge datasets
-    merged_ds <- merge_datasets(list(col_extract()))
-
-    # check columns selected
-    validate(need(cols, "Please select variables first."))
-
-    # lattice need at least 2 columns for the plot
-    validate(need(length(cols) >= 2, "Please select at least two variables."))
-
-
-    # check that data are available
-    validate(need(nrow(merged_ds) > 0, "There are zero observations in the (filtered) dataset."))
-
-    # check proper input values
-    validate(need(cex, "Need a proper cex value."))
-
-    # check proper input values
-    validate(need(alpha, "Need a proper alpha value."))
-
-    # reset chunks on every user-input change
+    ANL <- merged_data()$data()
     chunks_reset()
 
-    # set up expression chunk - lattice graph
-    chunks_push(
-      expression = quote(
-        merged_ds <- dplyr::mutate_if(merged_ds, is.character, as.factor)
-      )
-    )
+    alpha <- input$alpha
+    cex <- input$cex
+    cols_names <- unique(unname(do.call(c, merged_data()$columns_source))) #todo: remove names in merge_datasets()
+    validate(need(length(cols_names) > 1, "Need at least 2 columns."))
 
-    # set up expression chunk - lattice graph
-    chunks_push(
-      substituteDirect(
-        object = quote(
-          lattice::splom(merged_ds[, .cols], pch = 16, alpha = .alpha, cex = .cex)
-        ),
-        frame = list(.cols = cols, .alpha = alpha, .cex = cex)
-      )
-    )
+    # ANL <- dplyr::mutate_if(ANL, is.character, as.factor) # should not be done here, but as preprocessing of data #nolintr
 
-    p <- chunks_eval()
+    # create plot
+    chunks_push(bquote({
+      lattice::splom(ANL[, .(cols_names)], pch = 16, alpha = .(alpha), cex = .(cex))
+    }))
 
-    chunks_validate_is_ok()
-
-    p
+    safe_chunks_eval()
   })
 
   # show r code
   observeEvent(input$show_rcode, {
-
-    title <- paste0("Scatterplotmatrix of ",
-                    paste(get_dataset_prefixed_col_names(col_extract()),
-                          collapse = ", "))
+    title <- paste0(
+      "Scatterplotmatrix of ",
+      paste(merged_data()$cols, collapse = ", ")
+    )
 
     show_rcode_modal(
       title = "R Code for a Scatterplotmatrix",
       rcode = get_rcode(
         datasets = datasets,
-        merge_expression = "",
+        merge_expression = merged_data()$expr,
         title = title
       )
     )
