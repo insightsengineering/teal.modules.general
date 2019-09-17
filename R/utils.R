65e1e3d126eebb8e423bac065e3d663dff1e8dba
Wolf, Sebastian {MDBB~Basel} <sebastian.wolf.sw1@roche.com>
2019-09-17 10:21:20 +0200

    updated docs (#206)
    * updated docs
    
    * changed docs back
    
    * updated due to comment from @slamao
    
    * added example from @slamao
    
    * took out example from running

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,20 @@
+#' Call a function with a character vector for the \code{...} argument
+#'
+#' @param fun (\code{character}) Name of a function where the \code{...} argument
+#'   shall be replaced by values from \code{str_args}.
+#' @param str_args (\code{character}) A character vector that the function shall
+#'  be executed with
+#'
+#' @return: call (i.e. expression) of the function provided by \code{fun}
+#'  with arguments provided by \code{str_args}.
+#'
+#' @examples
+#' \dontrun{
+#'   a <- 1
+#'   b <- 2
+#'   call_fun_dots("sum", c("a", "b"))
+#'   eval(call_fun_dots("sum", c("a", "b")))
+#' }
 call_fun_dots <- function(fun, str_args) {
   do.call("call", c(list(fun), lapply(str_args, as.name)), quote = TRUE)
 }
