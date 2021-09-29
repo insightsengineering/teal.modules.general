a8bc371116bb6cd7eb68ce93bef02fe5aba48217
Sorin Voicu <61691256+sorinvoicu@users.noreply.github.com>
2021-09-29 16:13:13 +0300

    fix args behaviour (#166)
    * fix args behaviour
    
    * Update R/tm_file_viewer.R
    
    * Update R/tm_file_viewer.R
    
    Co-authored-by: Konrad Pagacz <konrad.pagacz@contractors.roche.com>

--- R/tm_file_viewer.R
+++ R/tm_file_viewer.R
@@ -37,7 +37,7 @@ tm_file_viewer <- function(label = "File Viewer Module",
 
   stop_if_not(
     is_character_single(label),
-    is_character_list(input_path),
+    is_character_list(input_path) || is_character_vector(input_path),
     list(
       vapply(input_path, function(x) file.exists(x) || valid_url(x), logical(1)),
       "Non-existant file or url path, please provide valid paths."
@@ -80,15 +80,16 @@ ui_viewer <- function(id, ...) {
 }
 
 srv_viewer <- function(input, output, session, datasets, input_path) {
+
   temp_dir <- tempfile()
   if (!dir.exists(temp_dir)) {
     dir.create(temp_dir, recursive = TRUE)
   }
   addResourcePath(basename(temp_dir), temp_dir)
 
-  test_path_text <- function(file_path) {
+  test_path_text <- function(selected_path) {
     out <- tryCatch({
-      readLines(con = file_path)
+      readLines(con = selected_path)
     },
     error = function(cond) {
       return(FALSE)
@@ -99,37 +100,37 @@ srv_viewer <- function(input, output, session, datasets, input_path) {
     )
   }
 
-  handle_connection_type <- function(file_path) {
-    file_extension <- tools::file_ext(file_path)
-    file_class <- suppressWarnings(file(file_path))
+  handle_connection_type <- function(selected_path) {
+    file_extension <- tools::file_ext(selected_path)
+    file_class <- suppressWarnings(file(selected_path))
     close(file_class)
-    output_text <- test_path_text(file_path)
+    output_text <- test_path_text(selected_path)
 
     if (class(file_class)[1] == "url") {
-      list(file_path = file_path, output_text = output_text)
+      list(selected_path = selected_path, output_text = output_text)
     } else {
       if (isFALSE(output_text[1]) || file_extension == "svg") {
         file.copy(
-          normalizePath(file_path, winslash = "/"),
+          normalizePath(selected_path, winslash = "/"),
           temp_dir
         )
-        file_path <- file.path(basename(temp_dir), basename(file_path))
+        selected_path <- file.path(basename(temp_dir), basename(selected_path))
       }
 
-      list(file_path = file_path, output_text = output_text)
+      list(selected_path = selected_path, output_text = output_text)
     }
   }
 
-  display_file <- function(file_path) {
-    con_type <- handle_connection_type(file_path)
-    file_extension <- tools::file_ext(file_path)
+  display_file <- function(selected_path) {
+    con_type <- handle_connection_type(selected_path)
+    file_extension <- tools::file_ext(selected_path)
 
     if (file_extension %in% c("png", "apng", "jpg", "jpeg", "svg", "gif", "webp", "bmp")) {
-      tags$img(src = con_type$file_path, alt = "file does not exist")
+      tags$img(src = con_type$selected_path, alt = "file does not exist")
     } else if (file_extension == "pdf") {
       tags$embed(
         style = "height:600px; width:100%",
-        src = con_type$file_path
+        src = con_type$selected_path
       )
     } else if (!isFALSE(con_type$output_text[1])) {
       tags$pre(paste0(con_type$output_text, collapse = "\n"))
@@ -140,31 +141,32 @@ srv_viewer <- function(input, output, session, datasets, input_path) {
 
   tree_list <- function(file_or_dir) {
     nested_list <- lapply(file_or_dir, function(y) {
-      isdir <- file.info(y)$isdir
+      file_class <- suppressWarnings(file(y))
+      close(file_class)
 
-      if (!isdir) {
-        structure(y, sticon = "file")
+      if (class(file_class)[[1]] != "url") {
+        isdir <- file.info(y)$isdir
+        if (!isdir) {
+          structure(y, sticon = "file")
+        } else {
+          files <- list.files(y, full.names = TRUE, include.dirs = TRUE)
+
+          out <- lapply(files, function(x) tree_list(x))
+          out <- unlist(out, recursive = F)
+          if (!is_empty(files)) names(out) <- basename(files)
+          out
+        }
       } else {
-        files <- list.files(y, full.names = TRUE, include.dirs = TRUE)
-
-        out <- lapply(files, function(x) tree_list(x))
-        out <- unlist(out, recursive = F)
-        if (!is_empty(files)) names(out) <- basename(files)
-        out
+        structure(y, sticon = "file")
       }
+
     })
     names(nested_list) <- file_or_dir
     nested_list
   }
 
   output$tree <- shinyTree::renderTree({
-    if (all(vapply(input_path, file.exists, FUN.VALUE = logical(1)))) {
-      tree_list(input_path)
-    } else {
-      input_path <- lapply(input_path, function(x) structure(x, sticon = "file"))
-      names(input_path) <- input_path
-      input_path
-    }
+    tree_list(input_path)
   })
 
   observeEvent(
@@ -173,12 +175,12 @@ srv_viewer <- function(input, output, session, datasets, input_path) {
     handlerExpr = {
       if (!is_empty(shinyTree::get_selected(input$tree))) {
         obj <- shinyTree::get_selected(input$tree, format = "names")[[1]]
-        file_path <- do.call("file.path", as.list(c(attr(obj, "ancestry"), obj[1])))
-        req(file_path)
+        selected_path <- do.call("file.path", as.list(c(attr(obj, "ancestry"), obj[1])))
+        req(selected_path)
 
         output$output <- renderUI({
-          validate(need(!isTRUE(file.info(file_path)$isdir), "Please select a single file."))
-          display_file(file_path)
+          validate(need(!isTRUE(file.info(selected_path)$isdir), "Please select a single file."))
+          display_file(selected_path)
         })
       }
     }
