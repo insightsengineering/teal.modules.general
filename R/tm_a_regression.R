a06f2aace0f5d1c3e5084c4b487d9b0b0d1476c8
Maciej Nasinski <nasinski.maciej@gmail.com>
2021-12-02 21:44:42 +0100

    docs

--- R/tm_a_regression.R
+++ R/tm_a_regression.R
@@ -24,7 +24,7 @@
 #' 6. Residuals vs Leverage
 #' 7. Cook's dist vs Leverage
 #'
-#' @param ggplot2_args optional, (`ggplot2_args`) object created by [`ggplot2_args()`]
+#' @param ggplot2_args optional, (`ggplot2_args`) object created by [`teal.devel::ggplot2_args()`]
 #'  with settings for all the plots or named list of `ggplot2_args` objects for plot-specific settings.
 #'  For more details see the help vignette:
 #'  `vignette("Custom ggplot2 arguments module", package = "teal.devel")`
