d834e3510f058506a0608ba2344e544eb7f21616
Maciej Nasinski <nasinski.maciej@gmail.com>
2021-12-16 08:27:40 +0100

    Update man-roxygen/ggplot2_args_multi.R
    Co-authored-by: Dawid Kałędkowski <6959016+gogonzo@users.noreply.github.com>

--- man-roxygen/ggplot2_args_multi.R
+++ man-roxygen/ggplot2_args_multi.R
@@ -2,6 +2,6 @@
 #'  with settings for all the plots or named list of `ggplot2_args` objects for plot-specific settings.
 #'  For more details see the help vignette:
 #'  `vignette("Custom ggplot2 arguments module", package = "teal.devel")`
-#'  List names should match the following:
+#'  List names should match the following:\cr
 #'  `c("default", <%=ggnames%>)`.
 #'  The argument is merged with options variable `teal.ggplot2_args` and default module setup.
