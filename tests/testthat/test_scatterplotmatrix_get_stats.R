0e41f55080ee6811b6094d07102202faebcf6add
Mahmoud Hallal <mahmoud.hallal@roche.com>
2021-12-22 15:26:45 +0100

    testthat::

--- tests/testthat/test_scatterplotmatrix_get_stats.R
+++ tests/testthat/test_scatterplotmatrix_get_stats.R
@@ -1,4 +1,4 @@
-test_that("get_scatterplotmatrix_stats() x-y numeric", {
+testthat::test_that("get_scatterplotmatrix_stats() x-y numeric", {
   set.seed(1)
   x <- runif(25, 0, 1)
   y <- runif(25, 0, 1)
@@ -6,16 +6,16 @@ test_that("get_scatterplotmatrix_stats() x-y numeric", {
   x_na[c(3, 10, 18)] <- NA
 
   corr <- get_scatterplotmatrix_stats(x_na, y, .f = cor.test, .f_args = list(method = "pearson"))
-  expect_true(is.character(corr))
+  testthat::expect_true(is.character(corr))
 
   corr <- get_scatterplotmatrix_stats(x_na, y, .f = cor.test, .f_args = list(method = "pearson", na.action = na.fail))
-  expect_true(corr == "NA")
+  testthat::expect_true(corr == "NA")
 
   corr <- get_scatterplotmatrix_stats(x, y, .f = cor.test, .f_args = list(method = "pearson", na.action = na.fail))
-  expect_true(corr != "NA")
+  testthat::expect_true(corr != "NA")
 })
 
-test_that("get_scatterplotmatrix_stats() x-y character", {
+testthat::test_that("get_scatterplotmatrix_stats() x-y character", {
   x <- LETTERS[runif(25, 0, 10)]
   y <- LETTERS[runif(25, 0, 10)]
 
@@ -23,9 +23,9 @@ test_that("get_scatterplotmatrix_stats() x-y character", {
   x_na[c(3, 10, 18)] <- NA
 
   corr <- get_scatterplotmatrix_stats(x, y, .f = cor.test, .f_args = list(method = "pearson"))
-  expect_true(startsWith(corr, "cor"))
+  testthat::expect_true(startsWith(corr, "cor"))
   corr <- get_scatterplotmatrix_stats(x, y, .f = cor.test, .f_args = list(method = "kendall"))
-  expect_true(startsWith(corr, "tau"))
+  testthat::expect_true(startsWith(corr, "tau"))
   corr <- get_scatterplotmatrix_stats(x, y, .f = cor.test, .f_args = list(method = "spearman"))
-  expect_true(startsWith(corr, "rho"))
+  testthat::expect_true(startsWith(corr, "rho"))
 })
