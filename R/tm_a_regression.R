2c997d2ab7366774588974c3c555244e0086dc9b
Maciej Nasinski <nasinski.maciej@gmail.com>
2021-12-22 13:59:19 +0100

    polish2

--- R/tm_a_regression.R
+++ R/tm_a_regression.R
@@ -134,9 +134,8 @@ tm_a_regression <- function(label = "Regression Analysis",
   checkmate::assert_numeric(plot_height[1], lower = plot_height[2], upper = plot_height[3], .var.name = "plot_height")
   checkmate::assert_numeric(plot_width, len = 3, any.missing = FALSE, null.ok = TRUE, finite = TRUE)
   checkmate::assert_numeric(plot_width[1],
-    lower = plot_width[2], upper = plot_width[3], null.ok = TRUE,
-    .var.name = "plot_width"
-  )
+                            lower = plot_width[2], upper = plot_width[3], null.ok = TRUE,
+                            .var.name = "plot_width")
 
   # Send ui args
   args <- as.list(environment())
