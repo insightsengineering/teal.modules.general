c7ea6e1f18fc56366ea30bb7f79a5df141ee42b4
kpagacz <konrad.pagacz@contractors.roche.com>
2022-01-11 09:39:16 +0100

    fixed a note

--- R/tm_variable_browser.R
+++ R/tm_variable_browser.R
@@ -801,7 +801,7 @@ plot_var_summary <- function(var,
         just = c("left", "top")
       )
     } else {
-      var <- if (isTRUE(remove_NA_hist)) as.vector(na.omit(var)) else var
+      var <- if (isTRUE(remove_NA_hist)) as.vector(stats::na.omit(var)) else var
       ggplot(data.frame(var), aes(x = forcats::fct_infreq(as.factor(var)))) +
         geom_bar(stat = "count", aes(fill = ifelse(is.na(var), "withcolor", "")), show.legend = FALSE) +
         scale_fill_manual(values = c("gray50", "tan"))
