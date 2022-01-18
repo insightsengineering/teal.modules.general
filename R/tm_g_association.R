134d1365df474d0e06ae43af9c6a795bab398275
Maciej Nasinski <nasinski.maciej@gmail.com>
2022-01-18 09:53:58 +0100

    association

--- R/tm_g_association.R
+++ R/tm_g_association.R
@@ -10,14 +10,12 @@
 #'   associated variables.
 #' @param show_association optional, (`logical`) Whether show association of `vars`
 #'   with reference variable. Defaults to `TRUE`.
-#' @param distribution_theme optional, (`character`) `ggplot` Theme to be used by default
-#'   for the distribution plot. All themes can be chosen by the user. Defaults to `gray`.
-#'   `gg_themes` is defined internally as
-#'   `c("gray", "bw", "linedraw", "light", "dark", "minimal", "classic", "void", "test")`
-#' @param association_theme optional, (`character`) `ggplot` Theme to be used by default
-#'   for the association plots. All themes can be chosen by the user. Defaults to `gray`.
-#'   `gg_themes` is defined internally as
-#'   `c("gray", "bw", "linedraw", "light", "dark", "minimal", "classic", "void", "test")`
+#' @param distribution_theme optional, (\code{character}) \code{ggplot} Theme to be used by default.
+#'   One of \code{c("gray", "bw", "linedraw", "light", "dark", "minimal", "classic", "void", "test")}.
+#'   Each of the theme can be chosen by the user during the session. Defaults to \code{gray}.
+#' @param association_theme optional, (\code{character}) \code{ggplot} Theme to be used by default.
+#'   One of \code{c("gray", "bw", "linedraw", "light", "dark", "minimal", "classic", "void", "test")}.
+#'   Each of the theme can be chosen by the user during the session. Defaults to \code{gray}.
 #'
 #' @templateVar ggnames "Bivariate1", "Bivariate2"
 #' @template ggplot2_args_multi
