fcc409e1e43e1824dfcfb94d6a2eac74b647c837
Junlue Zhao <jzhao31415@gmail.com>
2022-01-25 08:59:31 -0600

    argument order

--- R/tm_missing_data.R
+++ R/tm_missing_data.R
@@ -30,8 +30,9 @@
 #'   modules = root_modules(
 #'     tm_missing_data(
 #'       ggplot2_args = list(
-#'         "Combinations Hist" = teal.devel::ggplot2_args(labs = list(caption = NULL)),
-#'         "Combinations Main" = teal.devel::ggplot2_args(labs = list(title = "Plot produced by Missing Data Module"))
+#'         "Combinations Hist" =
+#'           teal.devel::ggplot2_args(labs = list(title = "Plot produced by Missing Data Module", caption = NULL)),
+#'         "Combinations Main" = teal.devel::ggplot2_args(labs = list(title = NULL))
 #'       )
 #'     )
 #'   )
