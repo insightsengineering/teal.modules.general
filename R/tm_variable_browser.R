f12253f3ed607dece629567f8d1121e0b23ddfbf
Mahmoud Hallal <86970066+mhallal1@users.noreply.github.com>
2022-07-27 11:13:17 +0200

    reporter cleaning (#430)
    * simple_reporter
    
    * remove space
    
    * review
    
    Co-authored-by: Nikolas Burkoff <nikolas.burkoff@roche.com>

--- R/tm_variable_browser.R
+++ R/tm_variable_browser.R
@@ -132,12 +132,7 @@ ui_variable_browser <- function(id,
         6,
         teal.widgets::white_small_well(
           ### Reporter
-          shiny::tags$div(
-            teal.reporter::add_card_button_ui(ns("addReportCard")),
-            teal.reporter::download_report_button_ui(ns("downloadButton")),
-            teal.reporter::reset_report_button_ui(ns("resetButton"))
-          ),
-          shiny::tags$br(),
+          teal.reporter::simple_reporter_ui(ns("simple_reporter")),
           ###
           div(
             class = "clearfix",
@@ -432,8 +427,9 @@ srv_variable_browser <- function(id, datasets, reporter, datasets_selected, ggpl
         card <- teal.reporter::TealReportCard$new()
         card$set_name("Variable Browser Plot")
         card$append_text("Variable Browser Plot", "header2")
-        card$append_text("Filter State", "header3")
-        card$append_fs(datasets$get_filter_state())
+        if (input$raw_or_filtered) {
+          card$append_fs(datasets$get_filter_state())
+        }
         card$append_text("Plot", "header3")
         card$append_plot(variable_plot_r(), dim = pws$dim())
         if (!comment == "") {
@@ -442,10 +438,7 @@ srv_variable_browser <- function(id, datasets, reporter, datasets_selected, ggpl
         }
         card
       }
-
-      teal.reporter::add_card_button_srv("addReportCard", reporter = reporter, card_fun = card_fun)
-      teal.reporter::download_report_button_srv("downloadButton", reporter = reporter)
-      teal.reporter::reset_report_button_srv("resetButton", reporter)
+      teal.reporter::simple_reporter_srv("simple_reporter", reporter = reporter, card_fun = card_fun)
     }
     ###
   })
