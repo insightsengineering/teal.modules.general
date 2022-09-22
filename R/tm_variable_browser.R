2f250abd4b9007a4ddc517708002734e3f5c93e0
Nikolas Burkoff <nikolas.burkoff@roche.com>
2022-09-22 10:01:59 +0100

    fix variable browser

--- R/tm_variable_browser.R
+++ R/tm_variable_browser.R
@@ -1047,7 +1047,12 @@ render_tab_header <- function(dataset_name, output, data) {
   dataset_ui_id <- paste0("dataset_summary_", dataset_name)
   output[[dataset_ui_id]] <- renderText({
     df <- data[[dataset_name]]()
-    key <- get_join_keys(data)$get(dataset_name)[[dataset_name]]
+    join_keys <- get_join_keys(data)
+    if (!is.null(join_keys)) {
+      key <- get_join_keys(data)$get(dataset_name)[[dataset_name]]
+    } else {
+      key <- NULL
+    }
     sprintf(
       "Dataset with %s unique key rows and %s variables",
       nrow(unique(`if`(length(key) > 0, df[, key, drop = FALSE], df))),
@@ -1123,7 +1128,11 @@ render_tab_table <- function(dataset_name, output, data, input, columns_names) {
 
         # get icons proper for the data types
         icons <- stats::setNames(teal.slice:::variable_types(df), colnames(df))
-        icons[intersect(get_join_keys(data)$get(dataset_name)[[dataset_name]], colnames(df))] <- "primary_key"
+
+        join_keys <- get_join_keys(data)
+        if (!is.null(join_keys)) {
+          icons[intersect(join_keys$get(dataset_name)[[dataset_name]], colnames(df))] <- "primary_key"
+        }
         icons <- variable_type_icons(icons)
 
         # generate sparklines
