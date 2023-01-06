69ef24494511d518bfebb0611e240d7a6610517a
Nikolas Burkoff <nikolas.burkoff@roche.com>
2023-01-06 11:14:27 +0000

    Remove some deprecated ggplot2 functions (#507)
    Removed `..density..` and `..count..` and `..prop..` from tmg - closes
    #508

--- R/tm_g_bivariate.R
+++ R/tm_g_bivariate.R
@@ -778,8 +778,8 @@ bivariate_ggplot_call <- function(x_class = c("NULL", "numeric", "integer", "fac
     } else {
       plot_call <- reduce_plot_call(
         plot_call,
-        quote(geom_histogram(bins = 30, aes(y = ..density..))),
-        quote(geom_density(aes(y = ..density..))),
+        quote(geom_histogram(bins = 30, aes(y = after_stat(density)))),
+        quote(geom_density(aes(y = after_stat(density)))),
         quote(ylab("Density"))
       )
     }
@@ -795,8 +795,8 @@ bivariate_ggplot_call <- function(x_class = c("NULL", "numeric", "integer", "fac
     } else {
       plot_call <- reduce_plot_call(
         plot_call,
-        quote(geom_histogram(bins = 30, aes(y = ..density..))),
-        quote(geom_density(aes(y = ..density..))),
+        quote(geom_histogram(bins = 30, aes(y = after_stat(density)))),
+        quote(geom_density(aes(y = after_stat(density)))),
         quote(ylab("Density"))
       )
     }
@@ -812,7 +812,7 @@ bivariate_ggplot_call <- function(x_class = c("NULL", "numeric", "integer", "fac
     } else {
       plot_call <- reduce_plot_call(
         plot_call,
-        quote(geom_bar(aes(y = ..prop.., group = 1))),
+        quote(geom_bar(aes(y = after_stat(prop), group = 1))),
         quote(ylab("Fraction"))
       )
     }
@@ -828,7 +828,7 @@ bivariate_ggplot_call <- function(x_class = c("NULL", "numeric", "integer", "fac
     } else {
       plot_call <- reduce_plot_call(
         plot_call,
-        quote(geom_bar(aes(y = ..prop.., group = 1))),
+        quote(geom_bar(aes(y = after_stat(prop), group = 1))),
         quote(ylab("Fraction"))
       )
     }
