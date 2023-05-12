cb283870dba5f3eded18f6f88db0c66c1a64043f
Dony Unardi <donyunardi@gmail.com>
2023-05-12 10:58:42 -0700

    fix #520 make count as globalVariables

--- R/teal.modules.general.R
+++ R/teal.modules.general.R
@@ -21,3 +21,6 @@ NULL
 # df <- data.frame(x = c("A", "B", "C", "A"), y = c("Z", "Z", "W", "W"))
 # ggplot(df) +  ggmosaic::geom_mosaic(aes(x = ggmosaic::product(x), fill = y))
 # nolint end
+
+# Needed to avoid R CMD note on no visible binding
+utils::globalVariables("count")
