49d0c1b62363ed1ee642e1aec395636a398e73f9
m7pr <marcin.kosinski.mk1@roche.com>
2023-06-27 13:47:30 +0200

    fix #211

--- R/teal.modules.general.R
+++ R/teal.modules.general.R
@@ -23,4 +23,8 @@ NULL
 # nolint end
 
 # Needed to avoid R CMD note on no visible binding
-utils::globalVariables("count")
+utils::globalVariables(c(
+  "count", ".", "ADT", "ADY", "AENDT", "AENDY", "ANRIND", "ASTDT", "ASTDY", "AVAL", "AVALC", "AVALU", "AVISITN",
+  "BASETYPE", "DTHDT", "LBSEQ", "LBTESTCD", "LSTALVDT", "PARAM", "PARAMCD", "PARCAT1", "PARCAT2",
+  "STUDYID", "USUBJID", "conmed_level", "conmed_var", "conmed_var_name", "line_col", "line_col_legend", "line_col_opt"
+))
