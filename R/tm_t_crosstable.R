f2f1d1576b4c1ab65962093b9d5ca184e5640ff4
unknown <kirar.kartikeya1@gmail.com>
2023-10-04 14:18:28 +0530

    add card_template function and allow to pass label to reporter card content

--- R/tm_t_crosstable.R
+++ R/tm_t_crosstable.R
@@ -352,10 +352,13 @@ srv_t_crosstable <- function(id, data, reporter, filter_panel_api, label, x, y, 
     ### REPORTER
     if (with_reporter) {
       card_fun <- function(comment) {
-        card <- teal::TealReportCard$new()
-        card$set_name("Cross Table")
-        card$append_text("Cross Table", "header2")
-        if (with_filter) card$append_fs(filter_panel_api$get_filter_state())
+        card <- card_template(
+          title = "Cross Table",
+          label = label,
+          description = NULL,
+          with_filter = with_filter,
+          filter_panel_api = filter_panel_api
+        )
         card$append_text("Table", "header3")
         card$append_table(table_r())
         if (!comment == "") {
