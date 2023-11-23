f37846a6e331434acfa1a917503d4e9085105fc5
kartikeya kirar <kirar.kartikeya1@gmail.com>
2023-11-23 14:46:25 +0530

    `tdata` to `teal_data` - `tm_g_scatterplotmatrix` (#611)
    Example App
    
    ```
    data <- teal_data()
    data <- within(data, {
      ADSL <- teal.modules.general::rADSL
      ADRS <- teal.modules.general::rADRS
    })
    datanames <- c("ADSL", "ADRS")
    datanames(data) <- datanames
    join_keys(data) <- default_cdisc_join_keys[datanames]
    
    app <- teal::init(
      data = data,
      modules = teal::modules(
        teal.modules.general::tm_g_scatterplotmatrix(
          label = "Scatterplot matrix",
          variables = list(
            teal.transform::data_extract_spec(
              dataname = "ADSL",
              select = select_spec(
                label = "Select variables:",
                choices = variable_choices(data[["ADSL"]]),
                selected = c("AGE", "RACE", "SEX"),
                multiple = TRUE,
                ordered = TRUE,
                fixed = FALSE
              )
            ),
            teal.transform::data_extract_spec(
              dataname = "ADRS",
              filter = teal.transform::filter_spec(
                label = "Select endpoints:",
                vars = c("PARAMCD", "AVISIT"),
                choices = value_choices(data[["ADRS"]], c("PARAMCD", "AVISIT"), c("PARAM", "AVISIT")),
                selected = "INVET - END OF INDUCTION",
                multiple = TRUE
              ),
              select = select_spec(
                label = "Select variables:",
                choices = variable_choices(data[["ADRS"]]),
                selected = c("AGE", "AVAL", "ADY"),
                multiple = TRUE,
                ordered = TRUE,
                fixed = FALSE
              )
            )
          )
        )
      )
    )
    
    shinyApp(app$ui, app$server)
    ```

--- R/tm_g_scatterplotmatrix.R
+++ R/tm_g_scatterplotmatrix.R
@@ -163,7 +163,8 @@ ui_g_scatterplotmatrix <- function(id, ...) {
 srv_g_scatterplotmatrix <- function(id, data, reporter, filter_panel_api, variables, plot_height, plot_width) {
   with_reporter <- !missing(reporter) && inherits(reporter, "Reporter")
   with_filter <- !missing(filter_panel_api) && inherits(filter_panel_api, "FilterPanelAPI")
-  checkmate::assert_class(data, "tdata")
+  checkmate::assert_class(data, "reactive")
+  checkmate::assert_class(isolate(data()), "teal_data")
   moduleServer(id, function(input, output, session) {
     selector_list <- teal.transform::data_extract_multiple_srv(
       data_extract = list(variables = variables),
@@ -180,13 +181,12 @@ srv_g_scatterplotmatrix <- function(id, data, reporter, filter_panel_api, variab
 
     anl_merged_input <- teal.transform::merge_expression_srv(
       datasets = data,
-      join_keys = teal.data::join_keys(data),
       selector_list = selector_list
     )
 
     anl_merged_q <- reactive({
       req(anl_merged_input())
-      teal.code::new_qenv(tdata2env(data), code = get_code_tdata(data)) %>%
+      data() %>%
         teal.code::eval_code(as.expression(anl_merged_input()$expr))
     })
 
