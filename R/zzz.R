d7b3b20486c839bee38a733e5382e875bb8640c0
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2024-02-14 17:38:19 +0100

    add global variable that lists available ggplot2 themes

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,5 @@
 .onLoad <- function(libname, pkgname) { # nolint
   teal.logger::register_logger(namespace = "teal.modules.general")
 }
+
+ggplot_themes <- c("gray", "bw", "linedraw", "light", "dark", "minimal", "classic", "void")
