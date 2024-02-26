a80fb9dd6a81421a17e1957b3b95dabfbe991518
go_gonzo <dawid.kaledkowski@gmail.com>
2024-02-26 15:17:58 +0100

    fix comments

--- R/tm_missing_data.R
+++ R/tm_missing_data.R
@@ -122,7 +122,7 @@ tm_missing_data <- function(label = "Missing data",
   )
 }
 
-# UI function for the missing data module
+# UI function for the missing data module (all datasets)
 ui_page_missing_data <- function(id, pre_output = NULL, post_output = NULL) {
   ns <- NS(id)
   shiny::tagList(
@@ -147,7 +147,7 @@ ui_page_missing_data <- function(id, pre_output = NULL, post_output = NULL) {
   )
 }
 
-# Server function for the missing data module
+# Server function for the missing data module (all datasets)
 srv_page_missing_data <- function(id, data, reporter, filter_panel_api, parent_dataname,
                                   plot_height, plot_width, ggplot2_args, ggtheme) {
   moduleServer(id, function(input, output, session) {
@@ -234,7 +234,7 @@ srv_page_missing_data <- function(id, data, reporter, filter_panel_api, parent_d
   })
 }
 
-# UI function for the missing data module
+# UI function for the missing data module (single dataset)
 ui_missing_data <- function(id, by_subject_plot = FALSE) {
   ns <- NS(id)
 
@@ -304,6 +304,7 @@ ui_missing_data <- function(id, by_subject_plot = FALSE) {
   )
 }
 
+# UI encoding for the missing data module (all datasets)
 encoding_missing_data <- function(id, summary_per_patient = FALSE, ggtheme, datanames) {
   ns <- NS(id)
 
@@ -392,7 +393,7 @@ encoding_missing_data <- function(id, summary_per_patient = FALSE, ggtheme, data
   )
 }
 
-# Server function for the missing data module
+# Server function for the missing data (single dataset)
 srv_missing_data <- function(id, data, reporter, filter_panel_api, dataname, parent_dataname,
                              plot_height, plot_width, ggplot2_args) {
   with_reporter <- !missing(reporter) && inherits(reporter, "Reporter")
