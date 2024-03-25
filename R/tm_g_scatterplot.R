f5c1c2d3fb0620f459b2240f5349c70d51958d52
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2024-03-25 13:30:21 +0100

    module bookmarkable: tm_g_scatterplot

--- R/tm_g_scatterplot.R
+++ R/tm_g_scatterplot.R
@@ -319,7 +319,7 @@ tm_g_scatterplot <- function(label = "Scatterplot",
     ),
     datanames = teal.transform::get_extract_datanames(data_extract_list)
   )
-  attr(ans, "teal_bookmarkable") <- NULL
+  attr(ans, "teal_bookmarkable") <- TRUE
   ans
 }
 
