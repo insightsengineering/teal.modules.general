c19e8a2b143135bdaa512675a5c34ae61eb6f442
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-04-08 12:58:29 +0200

    tests: adds CDISC data helper for e2e tests

--- tests/testthat/helper-TealAppDriver.R
+++ tests/testthat/helper-TealAppDriver.R
@@ -4,9 +4,23 @@ TealAppDriver <- getFromNamespace("TealAppDriver", "teal")
 # Helper function
 simple_teal_data <- function() {
   data <- within(teal.data::teal_data(), {
+    require(nestcolor)
     iris <- iris
     mtcars <- mtcars
   })
   teal.data::datanames(data) <- c("iris", "mtcars")
+  data
+}
+
+simple_cdisc_data <- function(datasets = c("ADSL", "ADRS", "ADTTE")) {
+  datasets <- match.arg(datasets, several.ok = TRUE)
+  data <- within(teal.data::teal_data(), {
+    require(nestcolor)
+    ADSL <- teal.modules.general::rADSL
+    ADRS <- teal.modules.general::rADRS
+    ADTTE <- teal.modules.general::rADTTE
+  })
+
+  teal.data::datanames(data) <- datasets
   data
 }
