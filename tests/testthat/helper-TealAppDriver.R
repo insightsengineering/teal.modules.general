1cf641f5a675b58700eeb8e792037aeafc65b87f
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-04-10 11:44:42 +0200

    Adds necessary shinytest2 helpers for automated tests (#714)
    # Pull Request
    
    Part of #712
    
    #### Changes description
    
    - Allow `TealAppDriver` to be used in tests
      - R6 class is not yet exported from `{teal}` and needs to be used here
    - Adds CDISC simple data helper to use in e2e tests
    - Adds `skip_if_too_deep` function
    
    ---------
    
    Signed-off-by: André Veríssimo <211358+averissimo@users.noreply.github.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ tests/testthat/helper-TealAppDriver.R
@@ -0,0 +1,29 @@
+# Import non-exported TealAppDriver from `teal` package
+TealAppDriver <- getFromNamespace("TealAppDriver", "teal") # nolint: object_name.
+
+# Helper function
+simple_teal_data <- function() {
+  data <- within(teal.data::teal_data(), {
+    require(nestcolor)
+    iris <- iris
+    mtcars <- mtcars
+  })
+  teal.data::datanames(data) <- c("iris", "mtcars")
+  data
+}
+
+simple_cdisc_data <- function(datasets = c("ADSL", "ADRS", "ADTTE")) {
+  datasets <- match.arg(datasets, several.ok = TRUE)
+  data <- within(
+    teal.data::teal_data(),
+    {
+      require(nestcolor)
+      ADSL <- teal.modules.general::rADSL
+      ADRS <- teal.modules.general::rADRS
+      ADTTE <- teal.modules.general::rADTTE
+    }
+  )
+  teal.data::datanames(data) <- datasets
+  teal.data::join_keys(data) <- teal.data::default_cdisc_join_keys[datasets]
+  data
+}
