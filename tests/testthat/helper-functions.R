e5c37835b2f4e6bd85755a48c374564a98f9f70e
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-04-11 14:32:02 +0200

    feat: move app driver initialization to helper

--- tests/testthat/helper-functions.R
+++ tests/testthat/helper-functions.R
@@ -10,3 +10,31 @@ mock_data_extract_spec <- function(dataname = "MOCK_DATASET",
     )
   )
 }
+
+# Based on example
+local_app_tm_a_pca <- function() {
+  data <- within(teal.data::teal_data(), {
+    require(nestcolor)
+
+    USArrests <- USArrests # nolint: object_name.
+  })
+  datanames(data) <- "USArrests"
+
+
+  TealAppDriver$new(
+    data = data,
+    modules = tm_a_pca(
+      dat = teal.transform::data_extract_spec(
+        dataname = "USArrests",
+        select = teal.transform::select_spec(
+          choices = teal.transform::variable_choices(
+            data = data[["USArrests"]],
+            c("Murder", "Assault", "UrbanPop", "Rape")
+          ),
+          selected = c("Murder", "Assault"),
+          multiple = TRUE
+        )
+      )
+    )
+  )
+}
--- tests/testthat/test-shinytest2-tm_a_pca.R
+++ tests/testthat/test-shinytest2-tm_a_pca.R
@@ -1,173 +1,127 @@
 testthat::test_that("e2e - tm_a_pca: data extract changes output", {
   skip_if_too_deep(5)
 
-  data <- within(teal.data::teal_data(), {
-    require(nestcolor)
-
-    USArrests <- USArrests # nolint: object_name.
-  })
-  datanames(data) <- "USArrests"
-
-  app <- TealAppDriver$new(
-    data = data,
-    modules = tm_a_pca(
-      dat = teal.transform::data_extract_spec(
-        dataname = "USArrests",
-        select = teal.transform::select_spec(
-          choices = teal.transform::variable_choices(
-            data = data[["USArrests"]],
-            c("Murder", "Assault", "UrbanPop", "Rape")
-          ),
-          selected = c("Murder", "Assault"),
-          multiple = TRUE
-        )
-      )
-    )
-  )
+  app_driver <- local_app_tm_a_pca()
 
   # Data selection (adds rows to tables)
-  app$set_module_input("dat-dataset_USArrests_singleextract-select", c("Murder", "Assault"), wait = FALSE)
-  app$wait_for_idle()
-  app$expect_no_validation_error()
+  app_driver$set_module_input("dat-dataset_USArrests_singleextract-select", c("Murder", "Assault"), wait = FALSE)
+  app_driver$wait_for_idle()
+  app_driver$expect_no_validation_error()
 
-  testthat::expect_match(app$get_active_module_output("tbl_eigenvector"), "Assault")
+  testthat::expect_match(app_driver$get_active_module_output("tbl_eigenvector"), "Assault")
 
   testthat::expect_failure(
-    testthat::expect_match(app$get_active_module_output("tbl_eigenvector"), "UrbanPop")
+    testthat::expect_match(app_driver$get_active_module_output("tbl_eigenvector"), "UrbanPop")
   )
 
-  app$set_module_input("dat-dataset_USArrests_singleextract-select", c("Murder", "UrbanPop"))
-  app$wait_for_idle()
-  app$expect_no_validation_error()
+  app_driver$set_module_input("dat-dataset_USArrests_singleextract-select", c("Murder", "UrbanPop"))
+  app_driver$wait_for_idle()
+  app_driver$expect_no_validation_error()
 
-  testthat::expect_match(app$get_active_module_output("tbl_eigenvector"), "UrbanPop")
+  testthat::expect_match(app_driver$get_active_module_output("tbl_eigenvector"), "UrbanPop")
 
   # Original Coordinate
-  app$set_module_input("plot_type", "Biplot")
-  app$set_module_input("variables", c("Murder"))
-  app$wait_for_idle()
-  app$expect_no_validation_error()
+  app_driver$set_module_input("plot_type", "Biplot")
+  app_driver$set_module_input("variables", c("Murder"))
+  app_driver$wait_for_idle()
+  app_driver$expect_no_validation_error()
 
   # Color by
-  app$set_module_input("response-dataset_USArrests_singleextract-select", c("Assault"))
-  app$wait_for_idle()
-  app$expect_no_validation_error()
+  app_driver$set_module_input("response-dataset_USArrests_singleextract-select", c("Assault"))
+  app_driver$wait_for_idle()
+  app_driver$expect_no_validation_error()
 
-  app$set_module_input("response-dataset_USArrests_singleextract-select", c("Murder"))
-  app$wait_for_idle()
-  app$expect_validation_error()
+  app_driver$set_module_input("response-dataset_USArrests_singleextract-select", c("Murder"))
+  app_driver$wait_for_idle()
+  app_driver$expect_validation_error()
 
-  app$stop()
+  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_a_pca: encodings update main panel", {
   skip_if_too_deep(5)
 
-  require(shinytest2)
-
-  data <- within(teal_data(), {
-    require(nestcolor)
-
-    USArrests <- USArrests # nolint: object_name.
-  })
-  datanames(data) <- "USArrests"
-
-  app <- TealAppDriver$new(
-    data = data,
-    modules = tm_a_pca(
-      dat = data_extract_spec(
-        dataname = "USArrests",
-        select = select_spec(
-          choices = variable_choices(
-            data = data[["USArrests"]],
-            c("Murder", "Assault", "UrbanPop", "Rape")
-          ),
-          selected = c("Murder", "Assault"),
-          multiple = TRUE
-        )
-      )
-    )
-  )
+  app_driver <- local_app_tm_a_pca()
 
   # Display section (hides tables)
 
-  app$set_module_input("tables_display", c())
-  app$expect_no_validation_error()
+  app_driver$set_module_input("tables_display", c())
+  app_driver$expect_no_validation_error()
 
-  testthat::expect_type(app$get_active_module_output("tbl_importance"), "list")
-  testthat::expect_setequal(names(app$get_active_module_output("tbl_importance")), c("message", "call", "type"))
+  testthat::expect_type(app_driver$get_active_module_output("tbl_importance"), "list")
+  testthat::expect_setequal(names(app_driver$get_active_module_output("tbl_importance")), c("message", "call", "type"))
 
-  testthat::expect_type(app$get_active_module_output("tbl_eigenvector"), "list")
-  testthat::expect_setequal(names(app$get_active_module_output("tbl_eigenvector")), c("message", "call", "type"))
+  testthat::expect_type(app_driver$get_active_module_output("tbl_eigenvector"), "list")
+  testthat::expect_setequal(names(app_driver$get_active_module_output("tbl_eigenvector")), c("message", "call", "type"))
 
   # Plot type (select each)
 
   # Changing input will trigger an output change
-  app$set_module_input("plot_type", "Circle plot")
-  app$expect_no_validation_error()
+  app_driver$set_module_input("plot_type", "Circle plot")
+  app_driver$expect_no_validation_error()
 
-  app$set_module_input("plot_type", "Biplot")
-  app$expect_no_validation_error()
+  app_driver$set_module_input("plot_type", "Biplot")
+  app_driver$expect_no_validation_error()
 
-  app$set_module_input("plot_type", "Eigenvector plot")
-  app$expect_no_validation_error()
+  app_driver$set_module_input("plot_type", "Eigenvector plot")
+  app_driver$expect_no_validation_error()
 
-  app$set_module_input("plot_type", "Elbow plot") # Initial value
-  app$expect_no_validation_error()
+  app_driver$set_module_input("plot_type", "Elbow plot") # Initial value
+  app_driver$expect_no_validation_error()
 
   # Pre-processing
 
-  app$set_module_input("standardization", "center")
-  app$set_module_input("standardization", "center_scale")
-  app$set_module_input("standardization", "none") # Initial value
+  app_driver$set_module_input("standardization", "center")
+  app_driver$set_module_input("standardization", "center_scale")
+  app_driver$set_module_input("standardization", "none") # Initial value
 
   # NA Action
 
-  app$set_module_input("na_action", "drop")
-  app$set_module_input("na_action", "none")
+  app_driver$set_module_input("na_action", "drop")
+  app_driver$set_module_input("na_action", "none")
 
   # Selected plot's specific settings is not visible
-  no_plot_settings_selector <- sprintf("#%s-%s %s", app$active_module_ns(), "plot_settings", "span.help-block")
-  x_axis_selector <- sprintf("#%s-%s", app$active_module_ns(), "x_axis")
+  no_plot_settings_selector <- sprintf("#%s-%s %s", app_driver$active_module_ns(), "plot_settings", "span.help-block")
+  x_axis_selector <- sprintf("#%s-%s", app_driver$active_module_ns(), "x_axis")
   color_by_selector <- sprintf(
     "#%s-%s",
-    app$active_module_ns(),
+    app_driver$active_module_ns(),
     "response-dataset_USArrests_singleextract-select_input"
   )
 
-  app$set_module_input("plot_type", "Elbow plot", wait = FALSE)
-  testthat::expect_true(app$is_visible(no_plot_settings_selector))
-  testthat::expect_false(app$is_visible(x_axis_selector))
-  testthat::expect_false(app$is_visible(color_by_selector))
+  app_driver$set_module_input("plot_type", "Elbow plot", wait = FALSE)
+  testthat::expect_true(app_driver$is_visible(no_plot_settings_selector))
+  testthat::expect_false(app_driver$is_visible(x_axis_selector))
+  testthat::expect_false(app_driver$is_visible(color_by_selector))
 
-  app$set_module_input("plot_type", "Circle plot", wait = FALSE)
-  testthat::expect_false(app$is_visible(no_plot_settings_selector))
-  testthat::expect_true(app$is_visible(x_axis_selector))
+  app_driver$set_module_input("plot_type", "Circle plot", wait = FALSE)
+  testthat::expect_false(app_driver$is_visible(no_plot_settings_selector))
+  testthat::expect_true(app_driver$is_visible(x_axis_selector))
 
-  app$set_module_input("plot_type", "Biplot", wait = FALSE)
-  testthat::expect_false(app$is_visible(no_plot_settings_selector))
-  testthat::expect_true(app$is_visible(x_axis_selector))
-  testthat::expect_true(app$is_visible(color_by_selector))
+  app_driver$set_module_input("plot_type", "Biplot", wait = FALSE)
+  testthat::expect_false(app_driver$is_visible(no_plot_settings_selector))
+  testthat::expect_true(app_driver$is_visible(x_axis_selector))
+  testthat::expect_true(app_driver$is_visible(color_by_selector))
 
   # Theme
 
-  app$set_module_input("ggtheme-selectized", "bw")
-  app$expect_no_validation_error()
-  app$set_module_input("ggtheme-selectized", "light")
-  app$expect_no_validation_error()
-  app$set_module_input("ggtheme-selectized", "dark")
-  app$expect_no_validation_error()
+  app_driver$set_module_input("ggtheme-selectized", "bw")
+  app_driver$expect_no_validation_error()
+  app_driver$set_module_input("ggtheme-selectized", "light")
+  app_driver$expect_no_validation_error()
+  app_driver$set_module_input("ggtheme-selectized", "dark")
+  app_driver$expect_no_validation_error()
 
   # Font size
 
-  app$set_module_input("font_size", "8")
-  app$expect_no_validation_error()
+  app_driver$set_module_input("font_size", "8")
+  app_driver$expect_no_validation_error()
 
-  app$set_module_input("font_size", "20")
-  app$expect_no_validation_error()
+  app_driver$set_module_input("font_size", "20")
+  app_driver$expect_no_validation_error()
 
-  app$set_module_input("font_size", "15")
-  app$expect_no_validation_error()
+  app_driver$set_module_input("font_size", "15")
+  app_driver$expect_no_validation_error()
 
-  app$stop()
+  app_driver$stop()
 })
