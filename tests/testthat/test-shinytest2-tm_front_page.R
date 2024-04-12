c22b26281b745cfff354c60e93ed1ef62b361dc2
unknown <kirar.kartikeya1@gmail.com>
2024-04-12 13:56:48 +0530

    restyling the tests

--- tests/testthat/test-shinytest2-tm_front_page.R
+++ tests/testthat/test-shinytest2-tm_front_page.R
@@ -1,20 +1,24 @@
 test_that("e2e: tm_front_page initializes without errors and html elements", {
-  #skip_if_too_deep(5)
+  skip_if_too_deep(5)
   app_driver <- app_driver_tm_front_page()
 
   app_driver$expect_no_shiny_error()
 
   # header text
-  testthat::expect_match(app_driver$get_text(selector = "#front_page_headers"),
-                         "Important information")
+  testthat::expect_match(
+    app_driver$get_text(selector = "#front_page_headers"),
+    "Important information"
+  )
   # additional tags
-  testthat::expect_match(app_driver$get_text(selector = "#front_page_custom_html"),
-                         "Additional HTML or shiny tags go here")
+  testthat::expect_match(
+    app_driver$get_text(selector = "#front_page_custom_html"),
+    "Additional HTML or shiny tags go here"
+  )
   app_driver$stop()
 })
 
 test_that("e2e: tm_front_page displays tables", {
-  #skip_if_too_deep(5)
+  skip_if_too_deep(5)
   app_driver <- app_driver_tm_front_page()
   # tables
   testthat::expect_match(app_driver$get_active_module_output("table_1"), "MTCARS")
@@ -26,7 +30,7 @@ test_that("e2e: tm_front_page displays tables", {
 })
 
 test_that("e2e: tm_front_page displays metadata", {
-  #skip_if_too_deep(5)
+  skip_if_too_deep(5)
   app_driver <- app_driver_tm_front_page()
   # show metadata
   app_driver$click(NS(app_driver$active_module_ns(), "metadata_button"))
@@ -34,5 +38,3 @@ test_that("e2e: tm_front_page displays metadata", {
 
   app_driver$stop()
 })
-
-
