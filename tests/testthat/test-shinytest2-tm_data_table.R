abc0ff968a51dd43bb76892a447afff320e04ea1
unknown <kirar.kartikeya1@gmail.com>
2024-04-12 15:18:37 +0530

    seperate the tests

--- tests/testthat/test-shinytest2-tm_data_table.R
+++ tests/testthat/test-shinytest2-tm_data_table.R
@@ -3,20 +3,36 @@ test_that("e2e: tm_front_page initializes without errors", {
   app_driver <- app_driver_tm_data_table()
 
   app_driver$expect_no_shiny_error()
+  app_driver$stop()
+})
 
-  # tables
-  testthat::expect_match(app_driver$get_active_module_output("iris-data_table"), "IRIS")
+test_that("e2e: tm_front_page displays data table", {
+  skip_if_too_deep(5)
+  app_driver <- app_driver_tm_data_table()
+
+  # table
+  testthat::expect_match(app_driver$get_active_module_output("iris-data_table"), "Table Caption")
   testthat::expect_true(app_driver$is_visible(selector = app_driver$active_module_element("iris-data_table")))
 
-  # variable selected
+  app_driver$stop()
+})
+
+test_that("e2e: tm_front_page variable selection", {
+  skip_if_too_deep(5)
+  app_driver <- app_driver_tm_data_table()
+
+  # default variable selection
   testthat::expect_equal(
-    app_driver$get_active_module_input("mtcars-variables"),
-    c("mpg", "cyl", "disp", "hp", "drat", "wt")
+    app_driver$get_active_module_input("iris-variables"),
+    c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species" )
   )
-  app_driver$set_module_input("mtcars-variables", c("vs", "am"))
+
+  # new variable selection
+  app_driver$set_module_input("iris-variables", c("Petal.Width", "Species"))
+  app_driver$expect_no_validation_error()
   testthat::expect_equal(
-    app_driver$get_active_module_input("mtcars-variables"),
-    c("vs", "am")
+    app_driver$get_active_module_input("iris-variables"),
+    c("Petal.Width", "Species")
   )
 
   app_driver$stop()
