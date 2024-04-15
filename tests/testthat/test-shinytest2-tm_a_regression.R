3f07bfeb3c07e4481485f271273801e5f708d5b5
m7pr <marcin.kosinski.mk1@roche.com>
2024-04-15 15:41:50 +0200

    check if values can be set

--- tests/testthat/test-shinytest2-tm_a_regression.R
+++ tests/testthat/test-shinytest2-tm_a_regression.R
@@ -116,20 +116,17 @@ testthat::test_that("e2e - tm_a_regerssion: clicking plot settings accordion pan
   app <- app_driver_tm_a_regression()
   app$expect_no_shiny_error()
 
-  testthat::expect_false(app$is_visible(app$active_module_element("size-label")))
-  testthat::expect_false(app$is_visible(app$active_module_element("alpha-label")))
-  testthat::expect_false(app$is_visible(app$active_module_element("label_min_segment-label")))
-  testthat::expect_false(app$is_visible(app$active_module_element("module-ggtheme-label")))
+  testthat::expect_false(app$is_visible(app$active_module_element("size")))
+  testthat::expect_false(app$is_visible(app$active_module_element("alpha")))
+  testthat::expect_false(app$is_visible(app$active_module_element("label_min_segment")))
+  testthat::expect_false(app$is_visible(app$active_module_element("module-ggtheme")))
 
   # After click they are visible.
   app$click(selector = "#_div > div.panel-heading.collapsed")
-  testthat::expect_true(app$is_visible(app$active_module_element("size-label")))
-  app$set_module_input("size-label", 3)
-  app$expect_no_validation_error()
-
-  testthat::expect_true(app$is_visible(app$active_module_element("alpha-label")))
-  testthat::expect_true(app$is_visible(app$active_module_element("label_min_segment-label")))
-  testthat::expect_true(app$is_visible(app$active_module_element("ggtheme-label")))
+  testthat::expect_true(app$is_visible(app$active_module_element("size")))
+  testthat::expect_true(app$is_visible(app$active_module_element("alpha")))
+  testthat::expect_true(app$is_visible(app$active_module_element("label_min_segment")))
+  testthat::expect_true(app$is_visible(app$active_module_element("ggtheme")))
 
   app$stop()
 })
@@ -146,6 +143,12 @@ testthat::test_that("e2e - tm_a_regerssion: plot settings have default values", 
   testthat::expect_identical(app$get_active_module_input("alpha"), 1)
   testthat::expect_identical(app$get_active_module_input("label_min_segment"), 0.5)
   testthat::expect_identical(app$get_active_module_input("ggtheme"), "gray")
+
+  app$set_module_input("size", 3)
+  app$set_module_input("alpha", 0.5)
+  app$set_module_input("label_min_segment", 0.6)
+  app$set_module_input("ggtheme", "bw")
+  app$expect_no_validation_error()
 
   app$stop()
 })
