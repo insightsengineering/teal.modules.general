c420f7eb0c97b9b4815f10ecb79d164233200c2a
m7pr <marcin.kosinski.mk1@roche.com>
2024-04-15 15:02:31 +0200

    gogonzo suggestions - divide tests into smaller tests

--- tests/testthat/test-shinytest2-tm_a_regression.R
+++ tests/testthat/test-shinytest2-tm_a_regression.R
@@ -1,4 +1,5 @@
-testthat::test_that("e2e - tm_a_regerssion: verify encoding values and widgets", {
+testthat::test_that("e2e - tm_a_regerssion:
+  data extract spec elements are initialized with the default values specified by response and regressor arg", {
   skip_if_too_deep(5)
 
   app <- app_driver_tm_a_regression()
@@ -9,13 +10,9 @@ testthat::test_that("e2e - tm_a_regerssion: verify encoding values and widgets",
     "Regression"
   )
 
-  # Check MAIN encoding panel
-  # DO WE NEED any function for checking top of the encoding panel part?
   encoding_dataset <- app$get_text("#teal-main_ui-root-regression .help-block")
   testthat::expect_match(encoding_dataset, "Dataset:[\n ]*CO2", all = FALSE)
 
-
-  # Check plot settings
   testthat::expect_identical(
     app$get_active_module_input("plot_type"),
     "Normal Q-Q"
@@ -26,39 +23,83 @@ testthat::test_that("e2e - tm_a_regerssion: verify encoding values and widgets",
     "conc"
   )
   app$set_module_input("regressor-dataset_CO2_singleextract-select", "Treatment")
-  app$expect_no_validation_error()
-  testthat::expect_identical(
-    app$get_active_module_input("regressor-dataset_CO2_singleextract-select"),
-    "Treatment"
-  )
+  app$stop()
+})
+
+testthat::test_that("e2e - tm_a_regerssion:
+  plot type has 7 specific choices & changing choices does not throw errors", {
+  skip_if_too_deep(5)
+
+  app <- app_driver_tm_a_regression()
+  app$expect_no_shiny_error()
 
   plot_types <- app$active_module_element_text("plot_type > div")
-  testthat::expect_match(plot_types, "Response vs Regressor", fixed = TRUE)
-  testthat::expect_match(plot_types, "Scale-Location", fixed = TRUE)
-  testthat::expect_match(plot_types, "Residuals vs Leverage", fixed = TRUE)
 
-  app$set_module_input("plot_type", "Residuals vs Fitted")
-  app$expect_no_validation_error()
-  app$set_module_input("plot_type", "Normal Q-Q")
-  app$expect_no_validation_error()
+  possible_choices <-
+    c("Response vs Regressor", "Scale-Location", "Residuals vs Leverage",
+      "Residuals vs Fitted", "Normal Q-Q", "Cook's distance", "Cook's dist vs Leverage")
 
-  # No outlier definition.
+  invisible(
+    lapply(
+      possible_choices,
+      function(choice) {
+        expect_match(plot_types, choice, fixed = TRUE)
+        app$set_module_input("plot_type", choice)
+        app$expect_no_validation_error()
+      }
+    )
+  )
+
+  app$stop()
+})
+
+testthat::test_that("e2e - tm_a_regerssion: outlier definition and label are visible by default", {
+  skip_if_too_deep(5)
+
+  app <- app_driver_tm_a_regression()
+  app$expect_no_shiny_error()
+
+  testthat::expect_true(app$get_active_module_input("show_outlier"))
+  testthat::expect_true(app$is_visible(app$active_module_element("label_var_input")))
+
+  app$stop()
+})
+
+testthat::test_that("e2e - tm_a_regerssion: unchecking display outlier hides outlier label and definition", {
+  skip_if_too_deep(5)
+
+  app <- app_driver_tm_a_regression()
+  app$expect_no_shiny_error()
+
   app$set_module_input("show_outlier", FALSE)
   testthat::expect_false(app$is_visible(app$active_module_element("outlier-label")))
+  testthat::expect_false(app$is_visible(app$active_module_element("label_var_input")))
+
   app$expect_no_validation_error()
 
-  # Bring back outlier definition.
-  app$set_module_input("show_outlier", TRUE)
-  testthat::expect_true(app$is_visible(app$active_module_element("outlier-label")))
-  app$expect_no_validation_error()
+  app$stop()
+})
 
-  # Plot settings are not visible.
+testthat::test_that("e2e - tm_a_regerssion: clicking plot settings accordion panel shows plot setting", {
+  skip_if_too_deep(5)
+
+  app <- app_driver_tm_a_regression()
+  app$expect_no_shiny_error()
+
   testthat::expect_false(app$is_visible(app$active_module_element("size-label")))
+  testthat::expect_false(app$is_visible(app$active_module_element("alpha-label")))
+  testthat::expect_false(app$is_visible(app$active_module_element("label_min_segment-label")))
+  testthat::expect_false(app$is_visible(app$active_module_element("module-ggtheme-label")))
+
   # After click they are visible.
   app$click(selector = "#_div > div.panel-heading.collapsed")
-  testthat::expect_true(app$is_visible(app$active_module_element("outlier-label")))
+  testthat::expect_true(app$is_visible(app$active_module_element("size-label")))
   app$set_module_input("size-label", 3)
   app$expect_no_validation_error()
+
+  testthat::expect_true(app$is_visible(app$active_module_element("alpha-label")))
+  testthat::expect_true(app$is_visible(app$active_module_element("label_min_segment-label")))
+  testthat::expect_true(app$is_visible(app$active_module_element("module-ggtheme-label")))
 
   app$stop()
 })
