17f0f005609720e46294b994d3641893d9d1811c
m7pr <marcin.kosinski.mk1@roche.com>
2024-04-16 15:50:35 +0200

    rename app to app_driver

--- tests/testthat/test-shinytest2-tm_a_regression.R
+++ tests/testthat/test-shinytest2-tm_a_regression.R
@@ -1,61 +1,61 @@
 testthat::test_that("e2e - tm_a_regerssion: data parameter and module label is passed properly", {
   skip_if_too_deep(5)
 
-  app <- app_driver_tm_a_regression()
-  app$expect_no_shiny_error()
+  app_driver <- app_driver_tm_a_regression()
+  app_driver$expect_no_shiny_error()
 
   testthat::expect_equal(
-    app$get_text("#teal-main_ui-root-active_tab > li.active > a"),
+    app_driver$get_text("#teal-main_ui-root-active_tab > li.active > a"),
     "Regression"
   )
 
-  encoding_dataset <- app$get_text("#teal-main_ui-root-regression .help-block")
+  encoding_dataset <- app_driver$get_text("#teal-main_ui-root-regression .help-block")
   testthat::expect_match(encoding_dataset, "Dataset:[\n ]*CO2", all = FALSE)
 
-  app$stop()
+  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_a_regerssion:
   data extract spec elements are initialized with the default values specified by response and regressor arg", {
   skip_if_too_deep(5)
 
-  app <- app_driver_tm_a_regression()
-  app$expect_no_shiny_error()
+  app_driver <- app_driver_tm_a_regression()
+  app_driver$expect_no_shiny_error()
 
   testthat::expect_identical(
-    app$active_module_element_text("response-dataset_CO2_singleextract-select_selected_text"),
+    app_driver$active_module_element_text("response-dataset_CO2_singleextract-select_selected_text"),
     "uptake"
   )
 
   testthat::expect_identical(
-    app$get_active_module_input("regressor-dataset_CO2_singleextract-select"),
+    app_driver$get_active_module_input("regressor-dataset_CO2_singleextract-select"),
     "conc"
   )
-  app$set_active_module_input("regressor-dataset_CO2_singleextract-select", "Treatment")
-  app$stop()
+  app_driver$set_active_module_input("regressor-dataset_CO2_singleextract-select", "Treatment")
+  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_a_regerssion: plot_type is set properly", {
   skip_if_too_deep(5)
 
-  app <- app_driver_tm_a_regression()
-  app$expect_no_shiny_error()
+  app_driver <- app_driver_tm_a_regression()
+  app_driver$expect_no_shiny_error()
 
   testthat::expect_identical(
-    app$get_active_module_input("plot_type"),
+    app_driver$get_active_module_input("plot_type"),
     "Normal Q-Q"
   )
-  app$stop()
+  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_a_regerssion:
   plot type has 7 specific choices & changing choices does not throw errors", {
   skip_if_too_deep(5)
 
-  app <- app_driver_tm_a_regression()
-  app$expect_no_shiny_error()
+  app_driver <- app_driver_tm_a_regression()
+  app_driver$expect_no_shiny_error()
 
-  plot_types <- app$active_module_element_text("plot_type > div")
+  plot_types <- app_driver$active_module_element_text("plot_type > div")
 
   possible_choices <-
     c(
@@ -68,40 +68,40 @@ testthat::test_that("e2e - tm_a_regerssion:
       possible_choices,
       function(choice) {
         expect_match(plot_types, choice, fixed = TRUE)
-        app$set_active_module_input("plot_type", choice)
-        app$expect_no_validation_error()
+        app_driver$set_active_module_input("plot_type", choice)
+        app_driver$expect_no_validation_error()
       }
     )
   )
 
-  app$stop()
+  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_a_regerssion: outlier definition and label are visible by default", {
   skip_if_too_deep(5)
 
-  app <- app_driver_tm_a_regression()
-  app$expect_no_shiny_error()
+  app_driver <- app_driver_tm_a_regression()
+  app_driver$expect_no_shiny_error()
 
-  testthat::expect_true(app$get_active_module_input("show_outlier"))
-  testthat::expect_true(app$is_visible(app$active_module_element("outlier-label")))
-  testthat::expect_true(app$is_visible(app$active_module_element("label_var_input")))
+  testthat::expect_true(app_driver$get_active_module_input("show_outlier"))
+  testthat::expect_true(app_driver$is_visible(app_driver$active_module_element("outlier-label")))
+  testthat::expect_true(app_driver$is_visible(app_driver$active_module_element("label_var_input")))
 
-  app$stop()
+  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_a_regerssion: outlier definition and label have default values and label text", {
   skip_if_too_deep(5)
 
-  app <- app_driver_tm_a_regression()
-  app$expect_no_shiny_error()
+  app_driver <- app_driver_tm_a_regression()
+  app_driver$expect_no_shiny_error()
 
   testthat::expect_match(
-    app$active_module_element_text("label_var_input"),
+    app_driver$active_module_element_text("label_var_input"),
     "Outlier label",
     fixed = TRUE
   )
-  outlier_label <- app$active_module_element_text("outlier-label")
+  outlier_label <- app_driver$active_module_element_text("outlier-label")
   testthat::expect_match(
     outlier_label,
     "Outlier definition:",
@@ -113,23 +113,23 @@ testthat::test_that("e2e - tm_a_regerssion: outlier definition and label have de
     fixed = TRUE
   )
 
-  testthat::expect_identical(app$get_active_module_input("label_var"), "uptake")
-  testthat::expect_identical(app$get_active_module_input("outlier"), 9L)
+  testthat::expect_identical(app_driver$get_active_module_input("label_var"), "uptake")
+  testthat::expect_identical(app_driver$get_active_module_input("outlier"), 9L)
 
-  app$stop()
+  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_a_regerssion: unchecking display outlier hides outlier label and definition", {
   skip_if_too_deep(5)
 
-  app <- app_driver_tm_a_regression()
-  app$expect_no_shiny_error()
+  app_driver <- app_driver_tm_a_regression()
+  app_driver$expect_no_shiny_error()
 
-  app$set_active_module_input("show_outlier", FALSE)
-  testthat::expect_false(app$is_visible(app$active_module_element("outlier-label")))
-  testthat::expect_false(app$is_visible(app$active_module_element("label_var_input")))
+  app_driver$set_active_module_input("show_outlier", FALSE)
+  testthat::expect_false(app_driver$is_visible(app_driver$active_module_element("outlier-label")))
+  testthat::expect_false(app_driver$is_visible(app_driver$active_module_element("label_var_input")))
 
-  app$expect_no_validation_error()
+  app_driver$expect_no_validation_error()
 
-  app$stop()
+  app_driver$stop()
 })
