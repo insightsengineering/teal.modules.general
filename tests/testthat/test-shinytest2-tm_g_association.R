623e01648af17ca56c1e88e6bbba7d564ad1041e
m7pr <marcin.kosinski.mk1@roche.com>
2024-04-16 15:48:45 +0200

    rename app to app_driver

--- tests/testthat/test-shinytest2-tm_g_association.R
+++ tests/testthat/test-shinytest2-tm_g_association.R
@@ -1,63 +1,63 @@
 testthat::test_that("e2e - tm_g_association: data parameter and module label is passed properly", {
   skip_if_too_deep(5)
 
-  app <- app_driver_tm_g_association()
-  app$expect_no_shiny_error()
+  app_driver <- app_driver_tm_g_association()
+  app_driver$expect_no_shiny_error()
 
   testthat::expect_equal(
-    app$get_text("#teal-main_ui-root-active_tab > li.active > a"),
+    app_driver$get_text("#teal-main_ui-root-active_tab > li.active > a"),
     "Association"
   )
 
-  encoding_dataset <- app$get_text("#teal-main_ui-root-association .help-block")
+  encoding_dataset <- app_driver$get_text("#teal-main_ui-root-association .help-block")
   testthat::expect_match(encoding_dataset, "Dataset:[\n ]*CO2", all = FALSE)
 
-  app$stop()
+  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_g_association:
   data extract spec elements are initialized with the default values specified by ref and vars arguments", {
   skip_if_too_deep(5)
-  app <- app_driver_tm_g_association()
+  app_driver <- app_driver_tm_g_association()
 
-  app$expect_no_shiny_error()
+  app_driver$expect_no_shiny_error()
 
   testthat::expect_identical(
-    app$get_active_module_input("ref-dataset_CO2_singleextract-select"),
+    app_driver$get_active_module_input("ref-dataset_CO2_singleextract-select"),
     "Plant"
   )
-  app$set_active_module_input("ref-dataset_CO2_singleextract-select", "Type")
-  app$wait_for_idle()
-  app$expect_no_validation_error()
+  app_driver$set_active_module_input("ref-dataset_CO2_singleextract-select", "Type")
+  app_driver$wait_for_idle()
+  app_driver$expect_no_validation_error()
 
   testthat::expect_identical(
-    app$get_active_module_input("vars-dataset_CO2_singleextract-select"),
+    app_driver$get_active_module_input("vars-dataset_CO2_singleextract-select"),
     "Treatment"
   )
-  app$set_active_module_input("vars-dataset_CO2_singleextract-select", "Plant")
-  app$wait_for_idle()
-  app$expect_no_validation_error()
+  app_driver$set_active_module_input("vars-dataset_CO2_singleextract-select", "Plant")
+  app_driver$wait_for_idle()
+  app_driver$expect_no_validation_error()
 
-  app$stop()
+  app_driver$stop()
 })
 
 
 testthat::test_that("e2e - tm_g_association: test if default radio buttons are checked", {
   skip_if_too_deep(5)
-  app <- app_driver_tm_g_association()
+  app_driver <- app_driver_tm_g_association()
 
-  app$expect_no_shiny_error()
+  app_driver$expect_no_shiny_error()
 
-  testthat::expect_true(app$get_active_module_input("association"))
-  testthat::expect_false(app$get_active_module_input("show_dist"))
-  testthat::expect_false(app$get_active_module_input("log_transformation"))
+  testthat::expect_true(app_driver$get_active_module_input("association"))
+  testthat::expect_false(app_driver$get_active_module_input("show_dist"))
+  testthat::expect_false(app_driver$get_active_module_input("log_transformation"))
 
-  app$set_active_module_input("association", FALSE)
-  app$set_active_module_input("show_dist", TRUE)
-  app$set_active_module_input("log_transformation", TRUE)
-  app$wait_for_idle()
-  app$expect_no_shiny_error()
+  app_driver$set_active_module_input("association", FALSE)
+  app_driver$set_active_module_input("show_dist", TRUE)
+  app_driver$set_active_module_input("log_transformation", TRUE)
+  app_driver$wait_for_idle()
+  app_driver$expect_no_shiny_error()
 
-  app$stop()
+  app_driver$stop()
 
 })
