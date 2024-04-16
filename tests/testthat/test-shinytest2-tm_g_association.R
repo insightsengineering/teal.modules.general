704c0a414d8d028fa81679e41c997089106e4bbd
m7pr <marcin.kosinski.mk1@roche.com>
2024-04-16 13:54:40 +0200

    rename `set_module_input` to `set_active_module_input`

--- tests/testthat/test-shinytest2-tm_g_association.R
+++ tests/testthat/test-shinytest2-tm_g_association.R
@@ -8,7 +8,7 @@ testthat::test_that("e2e - tm_g_association: ", {
     app$get_active_module_input("ref-dataset_CO2_singleextract-select"),
     "Plant"
   )
-  app$set_module_input("ref-dataset_CO2_singleextract-select", "Type")
+  app$set_active_module_input("ref-dataset_CO2_singleextract-select", "Type")
   app$expect_no_validation_error()
   testthat::expect_identical(
     app$get_active_module_input("ref-dataset_CO2_singleextract-select"),
@@ -19,7 +19,7 @@ testthat::test_that("e2e - tm_g_association: ", {
     app$get_active_module_input("vars-dataset_CO2_singleextract-select"),
     "Treatment"
   )
-  app$set_module_input("vars-dataset_CO2_singleextract-select", "Plant")
+  app$set_active_module_input("vars-dataset_CO2_singleextract-select", "Plant")
   app$expect_no_validation_error()
   testthat::expect_identical(
     app$get_active_module_input("vars-dataset_CO2_singleextract-select"),
@@ -30,9 +30,9 @@ testthat::test_that("e2e - tm_g_association: ", {
   testthat::expect_false(app$get_active_module_input("show_dist"))
   testthat::expect_false(app$get_active_module_input("log_transformation"))
 
-  app$set_module_input("association", FALSE)
-  app$set_module_input("show_dist", TRUE)
-  app$set_module_input("log_transformation", TRUE)
+  app$set_active_module_input("association", FALSE)
+  app$set_active_module_input("show_dist", TRUE)
+  app$set_active_module_input("log_transformation", TRUE)
   app$expect_no_shiny_error()
 
   # Plot settings are not visible.
@@ -40,8 +40,8 @@ testthat::test_that("e2e - tm_g_association: ", {
   # After click they are visible.
   app$click(selector = "#_div > div.panel-heading")
   testthat::expect_true(app$is_visible(app$active_module_element("swap_axes")))
-  app$set_module_input("swap_axes", TRUE)
-  app$set_module_input("rotate_xaxis_labels", TRUE)
+  app$set_active_module_input("swap_axes", TRUE)
+  app$set_active_module_input("rotate_xaxis_labels", TRUE)
   app$expect_no_validation_error()
 
   app$stop()
