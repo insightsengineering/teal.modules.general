991507eeedb11de0f126d44f90477c25406e021f
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-04-17 10:47:22 +0200

    Apply suggestions from @m7pr on `set_active_module_input` rename
    Co-authored-by: Marcin <133694481+m7pr@users.noreply.github.com>
    Signed-off-by: André Veríssimo <211358+averissimo@users.noreply.github.com>

--- tests/testthat/test-shinytest2-tm_g_response.R
+++ tests/testthat/test-shinytest2-tm_g_response.R
@@ -62,13 +62,13 @@ testthat::test_that("e2e - tm_g_response: encoding inputs produce output without
 
   app <- app_driver_tm_g_response()
 
-  app$set_module_input("response-dataset_ADSL_singleextract-select", "COUNTRY")
-  app$set_module_input("x-dataset_ADSL_singleextract-select", "SEX")
-  app$set_module_input("freq", "frequency")
-  app$set_module_input("count_labels", FALSE)
-  app$set_module_input("coord_flip", TRUE)
-  app$set_module_input("rotate_xaxis_labels", TRUE)
-  app$set_module_input("ggtheme", "light")
+  app$set_active_module_input("response-dataset_ADSL_singleextract-select", "COUNTRY")
+  app$set_active_module_input("x-dataset_ADSL_singleextract-select", "SEX")
+  app$set_active_module_input("freq", "frequency")
+  app$set_active_module_input("count_labels", FALSE)
+  app$set_active_module_input("coord_flip", TRUE)
+  app$set_active_module_input("rotate_xaxis_labels", TRUE)
+  app$set_active_module_input("ggtheme", "light")
 
   app$expect_no_validation_error()
 
