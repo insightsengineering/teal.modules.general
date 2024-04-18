55305e59dac67b4e442b885b241d696456ca439c
Marcin <133694481+m7pr@users.noreply.github.com>
2024-04-18 13:59:59 +0200

    Update tests/testthat/test-shinytest2-tm_g_association.R
    Co-authored-by: kartikeya kirar <kirar.kartikeya1@gmail.com>
    Signed-off-by: Marcin <133694481+m7pr@users.noreply.github.com>

--- tests/testthat/test-shinytest2-tm_g_association.R
+++ tests/testthat/test-shinytest2-tm_g_association.R
@@ -103,6 +103,7 @@ testthat::test_that("e2e - tm_g_association: test if default radio buttons are c
   app_driver$set_active_module_input("association", FALSE)
   app_driver$set_active_module_input("show_dist", TRUE)
   app_driver$set_active_module_input("log_transformation", TRUE)
+  app_driver$expect_no_validation_error()
   app_driver$expect_no_shiny_error()
 
   app_driver$stop()
