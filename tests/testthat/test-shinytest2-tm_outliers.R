411868f737629b31bd1e965418b11bf4eaf945b3
m7pr <marcin.kosinski.mk1@roche.com>
2024-04-18 12:46:41 +0200

    remove wait_fo_idle

--- tests/testthat/test-shinytest2-tm_outliers.R
+++ tests/testthat/test-shinytest2-tm_outliers.R
@@ -82,7 +82,6 @@ testthat::test_that("e2e - tm_outliers:
   app_driver$set_active_module_input("outlier_var-dataset_CO2_singleextract-select", "conc")
   app_driver$expect_no_shiny_error()
 
-  app_driver$wait_for_idle()
   testthat::expect_identical(
     app_driver$get_active_module_input("categorical_var-dataset_CO2_singleextract-filter1-col"),
     "Plant"
