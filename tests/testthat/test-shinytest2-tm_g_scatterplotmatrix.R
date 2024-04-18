5f803783a1e7def2b661ff2497493834d4657896
kartikeya kirar <kirar.kartikeya1@gmail.com>
2024-04-18 18:30:14 +0530

    Update tests/testthat/test-shinytest2-tm_g_scatterplotmatrix.R
    Co-authored-by: Marcin <133694481+m7pr@users.noreply.github.com>
    Signed-off-by: kartikeya kirar <kirar.kartikeya1@gmail.com>

--- tests/testthat/test-shinytest2-tm_g_scatterplotmatrix.R
+++ tests/testthat/test-shinytest2-tm_g_scatterplotmatrix.R
@@ -25,7 +25,7 @@ app_driver_tm_g_scatterplotmatrix <- function() { # nolint: object_length_linter
             selected = "INVET - END OF INDUCTION",
             multiple = TRUE
           ),
-          select = select_spec(
+          select = teal.transform::select_spec(
             label = "Select variables:",
             choices = variable_choices(data[["ADRS"]]),
             selected = c("AGE", "AVAL", "ADY"),
