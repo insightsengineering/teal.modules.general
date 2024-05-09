bda83c2a1d33cf11036fd77d8dff3e0f5f735631
m7pr <marcin.kosinski.mk1@roche.com>
2024-05-09 11:40:47 +0200

    test for tm_a_pca

--- R/tm_a_pca.R
+++ R/tm_a_pca.R
@@ -298,6 +298,9 @@ srv_a_pca <- function(id, data, reporter, filter_panel_api, dat, plot_height, pl
   checkmate::assert_class(data, "reactive")
   checkmate::assert_class(isolate(data()), "teal_data")
   moduleServer(id, function(input, output, session) {
+
+    logger::log_shiny_input_changes(input, namespace = "teal.modules.general")
+
     response <- dat
 
     for (i in seq_along(response)) {
