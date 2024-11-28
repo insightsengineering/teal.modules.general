426ef299fe0f7b6a29e56d54e101335309bb1902
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-11-28 15:46:36 +0000

    fix: linter error with long line

--- R/tm_missing_data.R
+++ R/tm_missing_data.R
@@ -13,7 +13,7 @@
 # nolint start: line_length.
 #' @param ggtheme (`character`) optional, specifies the default `ggplot2` theme for plots. Defaults to `classic`.
 #' @param ggplot2_args `r roxygen_ggplot2_args_param("Summary Obs", "Summary Patients", "Combinations Main", "Combinations Hist", "By Subject")`
-#' # nolint end: line_length.
+# nolint end: line_length.
 #' @param decorators `r roxygen_decorators_param("tm_missing_data")`
 #'
 #' @inherit shared_params return
