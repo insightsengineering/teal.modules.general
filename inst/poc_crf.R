b5884a2cda8153f9048a8bba28139a3a9c9bf1a8
vedhav <vedhaviyash4@gmail.com>
2024-12-02 20:59:47 +0530

    feat: use reactable

--- inst/poc_crf.R
+++ inst/poc_crf.R
@@ -3,6 +3,7 @@ pkgload::load_all("teal.widgets")
 pkgload::load_all("teal.modules.general")
 library(DT)
 library(labelled)
+library(reactable)
 
 # Note: Please add the `PATH_TO_DATA` and change the X, Y, and Z Administrations to the actual values in the data
 
@@ -35,7 +36,6 @@ data <- within(teal_data(), {
     mutate(subject = as.character(subject))
 })
 
-
 swim_plotly_specs <- list(
   list("plotly::add_markers", x = ~study_day, y = ~plot_subject, color = ~catagory, symbol = ~catagory, data = quote(study_drug_administration)),
   list("plotly::add_markers", x = ~study_day, y = ~plot_subject, color = ~catagory, symbol = ~catagory, data = quote(response_assessment)),
@@ -52,17 +52,17 @@ tm <- teal_transform_module(
           disposition <- swimlane_ds |>
             filter(!is.na(event_study_day)) |>
             filter(event_type == "disposition") |>
-            transmute(plot_subject, event_type, catagory = event_result, study_day = event_study_day)
+            transmute(subject, plot_subject, event_type, catagory = event_result, study_day = event_study_day)
 
           response_assessment <- swimlane_ds |>
             filter(!is.na(event_study_day)) |>
             filter(event_type == "response_assessment") |>
-            transmute(plot_subject, event_type, catagory = event_result, study_day = event_study_day)
+            transmute(subject, plot_subject, event_type, catagory = event_result, study_day = event_study_day)
 
           study_drug_administration <- swimlane_ds |>
             filter(!is.na(event_study_day)) |>
             filter(event_type == "study_drug_administration") |>
-            transmute(plot_subject, event_type, catagory = event_result, study_day = event_study_day)
+            transmute(subject, plot_subject, event_type, catagory = event_result, study_day = event_study_day)
 
           max_subject_day <- swimlane_ds |>
             group_by(plot_subject) |>
@@ -76,8 +76,8 @@ tm <- teal_transform_module(
 ui_mod <- function(id) {
   ns <- NS(id)
   fluidRow(
-    column(6, DTOutput(ns("mm_response"))),
-    column(6, DTOutput(ns("tx_listing")))
+    column(6, "MM Response", reactableOutput(ns("mm_response"))),
+    column(6, "", reactableOutput(ns("tx_listing")))
   )
 }
 
@@ -88,68 +88,111 @@ srv_mod <- function(id,
   checkmate::assert_class(data, "reactive")
   checkmate::assert_class(isolate(data()), "teal_data")
   moduleServer(id, function(input, output, session) {
-    output$test <- renderText({
-      print(plotly_selected)
-      "It works!"
-    })
-
-    output$mm_response <- renderDT({
-      select_cols <- c(
-        "subject", "visit_name", "visit_date", "form_name", "source_system_url_link",
-        "rspdn", "rspd", "rspd_study_day", "orsp", "bma", "bmb", "comnts"
-      )
-      new_col_names <- setNames(
-        select_cols,
-        c(
-          "Subject", "Visit Name", "Visit Date", "Form Name", "Source System URL Link",
-          "Assessment Performed", "Response Date", "Response Date Study Day",
-          "Response", "Best Marrow Aspirate", "Best Marrow Biopsy", "Comments"
-        )
-      )
+    output$mm_response <- renderReactable({
       swimlane_ds <- data()[["swimlane_ds"]]
+      col_defs <- list(
+        subject = colDef(name = "Subject"),
+        visit_name = colDef(name = "Visit Name"),
+        visit_date = colDef(name = "Visit Date"),
+        form_name = colDef(name = "Form Name"),
+        source_system_url_link = colDef(
+          name = "Source System URL Link",
+          cell = function(value) {
+            if (!is.na(value) && !is.null(value) && value != "") {
+              htmltools::tags$a(href = value, target = "_blank", "Link")
+            } else {
+              "N/A"
+            }
+          }
+        ),
+        rspdn = colDef(name = "Assessment Performed"),
+        rspd = colDef(name = "Response Date"),
+        rspd_study_day = colDef(name = "Response Date Study Day"),
+        orsp = colDef(name = "Response"),
+        bma = colDef(name = "Best Marrow Aspirate"),
+        bmb = colDef(name = "Best Marrow Biopsy"),
+        comnts = colDef(name = "Comments")
+      )
       mm_response <- swimlane_ds |>
         filter(event_study_day %in% plotly_selected()$x, plot_subject %in% plotly_selected()$y) |>
-        select(all_of(select_cols))
-      datatable(mm_response, colnames = new_col_names)
+        select(all_of(names(col_defs)))
+      reactable(
+        mm_response,
+        columns = col_defs,
+        defaultPageSize = 10,
+        searchable = TRUE,
+        sortable = TRUE
+      )
     })
 
-    output$tx_listing <- renderDT({
-      select_cols <- c(
-        "site_name", "subject", "visit_name", "visit_date", "form_name",
-        "source_system_url_link", "txnam", "txrec", "txrecrs", "txd_study_day",
-        "date_administered", "cydly", "cydlyrs", "cydlyae", "txdly", "txdlyrs",
-        "txdlyae", "txpdos", "txpdosu", "frqdv", "txrte", "txform", "txdmod",
-        "txrmod", "txdmae", "txad", "txadu", "txd", "txstm", "txstmu", "txed",
-        "txetm", "txetmu", "txtm", "txtmu", "txed_study_day", "infrt", "infrtu",
-        "tximod", "txirmod", "tximae"
-      )
-      new_col_names <- setNames(
-        select_cols,
-        c(
-          "Site Name", "Subject", "Visit Name", "Visit Date", "Form Name", "Source System URL Link",
-          "Study Drug Name", "Study Drug Administered", "Reason Study Drug Not Admin",
-          "Date Administered Study Day", "Date Administered", "Cycle Delay", "Cycle Delay Reason",
-          "Cycle Delay Adverse Event", "Dose Delay", "Dose Delay Reason", "AE related to Dose Delay",
-          "Planned Dose per Admin", "Planned Dose per Admin Unit", "Frequency", "Route of Administration",
-          "Dose Formulation", "Dose Modification", "Dose Modification Reason",
-          "AE related to Dose Modification", "Total Dose Administered", "Total Dose Administered Unit",
-          "Date Administered", "Start Time Administered", "Start Time Administered Unknown",
-          "End Date Administered", "End Time Administered", "End Time Administered Unknown",
-          "Time Administered", "Time Administered Unknown", "End Study Day", "Infusion Rate",
-          "Infusion Rate Unit", "Infusion Modified?", "Reason for Infusion modification",
-          "AE related to Infusion Modification"
-        )
-      )
+    output$tx_listing <- renderReactable({
       swimlane_ds <- data()[["swimlane_ds"]]
+
+      col_defs <- list(
+        site_name = colDef(name = "Site Name"),
+        subject = colDef(name = "Subject"),
+        visit_name = colDef(name = "Visit Name"),
+        visit_date = colDef(name = "Visit Date"),
+        form_name = colDef(name = "Form Name"),
+        source_system_url_link = colDef(
+          name = "Source System URL Link",
+          cell = function(value) {
+            if (!is.na(value) && !is.null(value) && value != "") {
+              htmltools::tags$a(href = value, target = "_blank", "Link")
+            } else {
+              "N/A"
+            }
+          }
+        ),
+        txnam = colDef(name = "Study Drug Name"),
+        txrec = colDef(name = "Study Drug Administered"),
+        txrecrs = colDef(name = "Reason Study Drug Not Admin"),
+        txd_study_day = colDef(name = "Date Administered Study Day"),
+        date_administered = colDef(name = "Date Administered"),
+        cydly = colDef(name = "Cycle Delay"),
+        cydlyrs = colDef(name = "Cycle Delay Reason"),
+        cydlyae = colDef(name = "Cycle Delay Adverse Event"),
+        txdly = colDef(name = "Dose Delay"),
+        txdlyrs = colDef(name = "Dose Delay Reason"),
+        txdlyae = colDef(name = "AE related to Dose Delay"),
+        txpdos = colDef(name = "Planned Dose per Admin"),
+        txpdosu = colDef(name = "Planned Dose per Admin Unit"),
+        frqdv = colDef(name = "Frequency"),
+        txrte = colDef(name = "Route of Administration"),
+        txform = colDef(name = "Dose Formulation"),
+        txdmod = colDef(name = "Dose Modification"),
+        txrmod = colDef(name = "Dose Modification Reason"),
+        txdmae = colDef(name = "AE related to Dose Modification"),
+        txad = colDef(name = "Total Dose Administered"),
+        txadu = colDef(name = "Total Dose Administered Unit"),
+        txd = colDef(name = "Date Administered"),
+        txstm = colDef(name = "Start Time Administered"),
+        txstmu = colDef(name = "Start Time Administered Unknown"),
+        txed = colDef(name = "End Date Administered"),
+        txetm = colDef(name = "End Time Administered"),
+        txetmu = colDef(name = "End Time Administered Unknown"),
+        txtm = colDef(name = "Time Administered"),
+        txtmu = colDef(name = "Time Administered Unknown"),
+        txed_study_day = colDef(name = "End Study Day"),
+        infrt = colDef(name = "Infusion Rate"),
+        infrtu = colDef(name = "Infusion Rate Unit"),
+        tximod = colDef(name = "Infusion Modified?"),
+        txirmod = colDef(name = "Reason for Infusion modification"),
+        tximae = colDef(name = "AE related to Infusion Modification")
+      )
       tx_listing <- swimlane_ds |>
         filter(event_study_day %in% plotly_selected()$x, plot_subject %in% plotly_selected()$y) |>
-        select(all_of(select_cols))
-      datatable(tx_listing, colnames = new_col_names)
+        select(all_of(names(col_defs)))
+      reactable(
+        tx_listing,
+        columns = col_defs,
+        defaultPageSize = 5,
+        searchable = TRUE,
+        sortable = TRUE
+      )
     })
   })
 }
-
-pkgload::load_all("teal.modules.general")
 
 spider_plotly_specs <- list(
   list("plotly::add_markers", x = ~event_study_day, y = ~event_result, color = ~subject, symbol = ~event_type, data = quote(spiderplot_ds)),
@@ -198,7 +241,9 @@ app <- init(
     tm_p_swimlane2(
       label = "Spiderplot",
       plotly_specs = spider_plotly_specs,
-      title = "Swimlane Efficacy Plot"
+      title = "Swimlane Efficacy Plot",
+      ui_mod = ui_mod,
+      srv_mod = srv_mod
     ),
     tm_data_table()
   ),
