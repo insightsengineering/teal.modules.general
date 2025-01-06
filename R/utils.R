4420127b5ea82976fda1a5f39380af534b80a7ab
André Veríssimo <211358+averissimo@users.noreply.github.com>
2025-01-06 14:23:18 +0000

    feat: initial copy of support functions

--- R/utils.R
+++ R/utils.R
@@ -299,21 +299,26 @@ srv_decorate_teal_data <- function(id, data, decorators, expr, expr_is_reactive 
 
   missing_expr <- missing(expr)
   if (!missing_expr && !expr_is_reactive) {
-    expr <- rlang::enexpr(expr)
+    expr <- dplyr::enexpr(expr) # Using dplyr re-export to avoid adding rlang to Imports
   }
 
   moduleServer(id, function(input, output, session) {
     decorated_output <- srv_transform_teal_data("inner", data = data, transformators = decorators)
 
     reactive({
-      # ensure original errors are displayed and `eval_code` is never executed with NULL
-      req(data(), decorated_output())
-      if (missing_expr) {
-        decorated_output()
-      } else if (expr_is_reactive) {
-        teal.code::eval_code(decorated_output(), expr())
+      data_out <- try(data(), silent = TRUE)
+      if (inherits(data_out, "qenv.error")) {
+        data()
       } else {
-        teal.code::eval_code(decorated_output(), expr)
+        # ensure original errors are displayed and `eval_code` is never executed with NULL
+        req(data(), decorated_output())
+        if (missing_expr) {
+          decorated_output()
+        } else if (expr_is_reactive) {
+          teal.code::eval_code(decorated_output(), expr())
+        } else {
+          teal.code::eval_code(decorated_output(), expr)
+        }
       }
     })
   })
@@ -394,7 +399,7 @@ select_decorators <- function(decorators, scope) {
 
 #' Convert flat list of `teal_transform_module` to named lists
 #'
-#' @param decorators (list of `teal_transform_module`) to normalize.
+#' @param decorators (list of `teal_transform_modules`) to normalize.
 #' @return A named list of lists with `teal_transform_module` objects.
 #' @keywords internal
 normalize_decorators <- function(decorators) {
