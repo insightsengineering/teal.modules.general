6ecf51187eb44e52dbe500d08872f6a8a621a31e
m7pr <marcin.kosinski.mk1@roche.com>
2025-01-14 15:36:06 +0100

    do not split documentation of decorators

--- R/utils.R
+++ R/utils.R
@@ -33,11 +33,11 @@
 #' - When the length of `size` is one: the plot point sizes will have a fixed size.
 #' - When the length of `size` is three: the plot points size are dynamically adjusted based on
 #' vector of `value`, `min`, and `max`.
+#' @param transformators (`list` of `teal_transform_module`) that will be applied to transform module's data input.
 #' @param decorators `r lifecycle::badge("experimental")`
 #' (`list` of `teal_transform_module`, named `list` of `teal_transform_module` or `NULL`) optional,
 #' if not `NULL`, decorator for tables or plots included in the module.
 #' When a named list of `teal_transform_module`, the decorators are applied to the respective output objects.
-#' @param transformators (`list` of `teal_transform_module`) that will be applied to transform module's data input.
 #'
 #' Otherwise, the decorators are applied to all objects, which is equivalent as using the name `default`.
 #'
