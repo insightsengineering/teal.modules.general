4f62e13addf2a23c990c2e70975a5d2653e9a74c
Dawid Kaledkowski <dawid.kaledkowski@roche.com>
2025-02-14 19:30:51 +0000

    wip modularize

--- R/tm_p_spiderplot.R
+++ R/tm_p_spiderplot.R
@@ -35,7 +35,7 @@ ui_p_spiderplot <- function(id, height) {
         style = "width: 50%",
         tagList(
           h4("Most Recent Resp and Best Resp"),
-          reactableOutput(ns("recent_resp"))
+          ui_t_reactable(ns("recent_resp"))
         )
       ),
       div(
@@ -51,12 +51,12 @@ ui_p_spiderplot <- function(id, height) {
         div(
           class = "simple-card",
           h4("Disease Assessment - SFLC"),
-          ui_t_reactable(ns("sflc_listing"))
+          reactable::reactableOutput(ns("sflc_listing"))
         ),
         div(
           class = "simple-card",
           h4("Disease Assessment - SPEP"),
-          ui_t_reactable(ns("spep_listing"))
+          reactable::reactableOutput(ns("spep_listing"))
         )
       ),
       div(
@@ -77,7 +77,8 @@ srv_p_spiderplot <- function(id,
                              filter_panel_api,
                              plot_height = 600) {
   moduleServer(id, function(input, output, session) {
-    spiderplot_ds <- reactive(data()[["spiderplot_ds"]])
+    dataname <- "spiderplot_ds"
+    spiderplot_ds <- reactive(data()[[dataname]])
     observeEvent(spiderplot_ds(), {
       event_types <- unique(spiderplot_ds()$event_type)
       updateSelectInput(
@@ -88,24 +89,25 @@ srv_p_spiderplot <- function(id,
     plotly_q <- reactive({
       data() |>
         within(
-          selected_event = input$event_type,
-          height = input$plot_height,
+          dataname = str2lang(dataname),
+          dataname_filtered = str2lang(sprintf("%s_filtered", dataname)),
           time_var = str2lang(time_var),
           subject_var = str2lang(subject_var),
           value_var = str2lang(value_var),
+          selected_event = input$event_type,
+          height = input$plot_height,
           expr = {
             y_title <- selected_event
-            spiderplot_ds_filtered <- spiderplot_ds |>
-              filter(event_type == selected_event)
+            dataname_filtered <- filter(dataname, event_type == selected_event)
 
             p <- plotly::plot_ly(source = "spiderplot", height = height) |>
               plotly::add_markers(
                 x = ~time_var, y = ~value_var, color = ~subject_var,
-                data = spiderplot_ds_filtered
+                data = dataname_filtered
               ) |>
               plotly::add_lines(
                 x = ~time_var, y = ~value_var, color = ~subject_var,
-                data = spiderplot_ds_filtered,
+                data = dataname_filtered,
                 showlegend = FALSE
               ) |>
               plotly::layout(
@@ -120,10 +122,7 @@ srv_p_spiderplot <- function(id,
     })
 
     output$plot <- plotly::renderPlotly({
-      plotly::event_register(
-        plotly_q()$p,
-        "plotly_selected"
-      )
+      plotly::event_register(plotly_q()$p, "plotly_selected")
     })
 
     plotly_selected <- reactive(plotly::event_data("plotly_selected", source = "spiderplot"))
@@ -134,129 +133,126 @@ srv_p_spiderplot <- function(id,
       "orsp", "bma", "bmb", "comnts"
     )
 
-    plotly_selected_subjects <- reactive({
-      data()[["spiderplot_ds"]] |>
-        filter(event_study_day %in% plotly_selected()$x, event_result %in% plotly_selected()$y) |>
-        pull(subject)
-    })
-
-    recent_resp_ds <- reactive({
-      data()[["spiderplot_ds"]] |>
-        filter(event_type == "latest_response_assessment") |>
-        filter(subject %in% plotly_selected_subjects()) |>
-        select(all_of(resp_cols))
-    })
-
-    output$recent_resp <- renderReactable({
-      req(plotly_selected_subjects())
-      reactable(
-        recent_resp_ds(),
-        # columns = resp_cols,
-        selection = "single",
-        onClick = "select",
-        defaultPageSize = 15,
-        wrap = FALSE,
-        rowClass = JS("
-            function(rowInfo) {
-            console.log(rowInfo);
-              if (rowInfo.selected) {
-                return 'selected-row';
-              }
-            }
-          ")
+    plotly_selected_q <- reactive({
+      req(plotly_selected())
+      within(
+        plotly_q(),
+        dataname = str2lang(dataname),  # todo: replace with argument
+        time_var = str2lang(time_var),
+        subject_var = subject_var,
+        value_var = str2lang(value_var),
+        time_vals = plotly_selected()$x,
+        value_vals = plotly_selected()$y,
+        expr = {
+          brushed_subjects <- dplyr::filter(
+            dataname, time_var %in% time_vals, value_var %in% value_vals
+          )[[subject_var]]
+        }
       )
     })
 
-    table_selected_subjects <- reactive({
-      selected_row <- getReactableState("recent_resp", "selected")
-      if (!is.null(selected_row)) {
-        recent_resp_ds()[selected_row, ]$subject
-      } else {
-        unique(recent_resp_ds()$subject)
-      }
-    })
-
-    all_resp <- reactive({
-      data()[["spiderplot_ds"]] |>
-        filter(event_type == "response_assessment") |>
-        select(all_of(resp_cols)) |>
-        filter(subject %in% plotly_selected_subjects()) |>
-        filter(subject %in% table_selected_subjects())
-    })
-
-    output$all_resp <- renderReactable({
-      if (nrow(all_resp()) == 0) {
-        return()
-      }
-
-      reactable(
-        all_resp(),
-        # columns = resp_cols,
-        defaultPageSize = 15,
-        wrap = FALSE
+    recent_resp_q <- reactive({
+      req(plotly_selected_q())
+      within(
+        plotly_selected_q(),
+        dataname = str2lang(dataname),
+        subject_var = str2lang(subject_var),
+        resp_cols = resp_cols,
+        expr = {
+          recent_resp <- dplyr::filter(
+            dataname,
+            event_type == "latest_response_assessment",
+            subject_var %in% brushed_subjects # todo: figure this out
+          ) |>
+            select(all_of(resp_cols))
+        }
       )
     })
-
-    spep_cols <- c(
-      "subject", "visit_name", "visit_date", "form_name", "source_system_url_link",
-      "rspdn", "rspd", "rspd_study_day", "orsp", "bma", "bmb", "comnts",
-      "asmntdn", "blq", "coldr", "cold_study_day", "coltm", "coltmu", "lrspep1",
-      "mprte_raw", "mprtec"
-    )
-
-    spep <- reactive({
-      data()[["spiderplot_ds"]] |>
-        filter(event_type == "Serum M-protein") |>
-        filter(subject %in% table_selected_subjects()) |>
-        select(all_of(spep_cols))
-    })
-
-    output$spep_listing <- renderReactable({
-      if (nrow(spep()) == 0) {
-        return()
-      }
-
-      reactable(
-        spep(),
-        # columns = spep_cols,
-        defaultPageSize = 5,
-        wrap = FALSE
-      )
-    })
-
-
-    sflc_cols <- c(
-      "subject", "visit_name", "visit_date", "form_name", "source_system_url_link", "rspdn", "rspd",
-      "rspd_study_day", "orsp", "bma", "bmb", "comnts", "asmntdn", "blq", "coldr", "cold_study_day",
-      "coltm", "coltmu", "lchfrc", "lchfr_raw", "klchf_raw", "llchf_raw",
-      "klchp_raw", "mprte_raw", "mprtec"
-    )
-
-    sflc <- reactive({
-      data()[["spiderplot_ds"]] |>
-        filter(
-          event_type %in% c(
-            "Kappa free light chain quantity",
-            "Lambda free light chain quantity",
-            "Kappa-Lambda free light chain ratio"
+    
+    recent_resp_selected_q <- srv_t_reactable("recent_resp", data = recent_resp_q, dataname = "recent_resp")
+    # 
+    all_resp_q <- reactive({
+      req(nrow(recent_resp_selected_q()[["recent_resp_selected"]]))
+      within(
+        recent_resp_selected_q(),
+        dataname = str2lang(dataname),
+        subject_var = str2lang(subject_var),
+        subject_var_char = subject_var,
+        expr = {
+          all_resp <- filter(
+            dataname, 
+            event_type == "response_assessment",
+            subject_var == recent_resp_selected[[subject_var_char]]
           )
-        ) |>
-        filter(subject %in% table_selected_subjects()) |>
-        select(all_of(sflc_cols))
-    })
-
-    output$sflc_listing <- renderReactable({
-      if (nrow(sflc()) == 0) {
-        return()
-      }
-
-      reactable(
-        sflc(),
-        # columns = sflc_cols,
-        defaultPageSize = 5,
-        wrap = FALSE
+        }
       )
     })
+  
+    #todo: show all_resp only if recent_resp is selected  
+    srv_t_reactable("all_resp", data = all_resp_q, dataname = "all_resp") 
+
+    # 
+    # spep_cols <- c(
+    #   "subject", "visit_name", "visit_date", "form_name", "source_system_url_link",
+    #   "rspdn", "rspd", "rspd_study_day", "orsp", "bma", "bmb", "comnts",
+    #   "asmntdn", "blq", "coldr", "cold_study_day", "coltm", "coltmu", "lrspep1",
+    #   "mprte_raw", "mprtec"
+    # )
+    # 
+    # spep <- reactive({
+    #   req(table_selected_subjects())
+    #   data()[["spiderplot_ds"]] |>
+    #     filter(event_type == "Serum M-protein") |>
+    #     filter(subject %in% table_selected_subjects()) |>
+    #     select(all_of(spep_cols))
+    # })
+    # 
+    # output$spep_listing <- renderReactable({
+    #   if (nrow(spep()) == 0) {
+    #     return()
+    #   }
+    # 
+    #   reactable(
+    #     spep(),
+    #     # columns = spep_cols,
+    #     defaultPageSize = 5,
+    #     wrap = FALSE
+    #   )
+    # })
+    # 
+    # 
+    # sflc_cols <- c(
+    #   "subject", "visit_name", "visit_date", "form_name", "source_system_url_link", "rspdn", "rspd",
+    #   "rspd_study_day", "orsp", "bma", "bmb", "comnts", "asmntdn", "blq", "coldr", "cold_study_day",
+    #   "coltm", "coltmu", "lchfrc", "lchfr_raw", "klchf_raw", "llchf_raw",
+    #   "klchp_raw", "mprte_raw", "mprtec"
+    # )
+    # 
+    # sflc <- reactive({
+    #   data()[["spiderplot_ds"]] |>
+    #     filter(
+    #       event_type %in% c(
+    #         "Kappa free light chain quantity",
+    #         "Lambda free light chain quantity",
+    #         "Kappa-Lambda free light chain ratio"
+    #       )
+    #     ) |>
+    #     filter(subject %in% table_selected_subjects()) |>
+    #     select(all_of(sflc_cols))
+    # })
+    # 
+    # output$sflc_listing <- renderReactable({
+    #   if (nrow(sflc()) == 0) {
+    #     return()
+    #   }
+    # 
+    #   reactable(
+    #     sflc(),
+    #     # columns = sflc_cols,
+    #     defaultPageSize = 5,
+    #     wrap = FALSE
+    #   )
+    # })
   })
 }
 
--- R/tm_t_reactable.R
+++ R/tm_t_reactable.R
@@ -6,7 +6,7 @@ tm_t_reactables <- function(label = "Table", datanames, transformators = list(),
     ui = ui_t_reactable,
     srv = srv_t_reactable,
     ui_args = list(decorators = decorators),
-    srv_args = c(list(datanames = datanames, decorators = decorators), rlang::list(...))
+    srv_args = c(list(datanames = datanames, decorators = decorators), rlang::list(...)),
     datanames = datanames,
     transformers = transformers
   )
@@ -21,30 +21,53 @@ ui_t_reactable <- function(id) {
 }
 
 srv_t_reactable <- function(id, data, filter_panel_api, dataname, decorators, ...) {
-  moduleServer(id, function(input, output, session)) {
-    output$table <- reactable::renderReactable({
+  moduleServer(id, function(input, output, session) {
+    dataname_reactable <- sprintf("%s_reactable", dataname)
+    table_q <- reactive({
       req(data())
-      dataset <- data()[[dataname]]
-      args <- modifyList(
-        list(
-          dataset,
-          columns = teal.data::col_labels(dataset)
-          selection = "single",
-          onClick = "select",
-          defaultPageSize = 15,
-          wrap = FALSE,
-          rowClass = JS("
+      within(
+        data(),
+        dataname_reactable = str2lang(dataname_reactable),
+        dataname = str2lang(dataname),
+        {
+          dataname_reactable <- reactable::reactable(
+            dataname,
+            #columns = teal.data::col_labels(dataset), # todo: replace with labels
+            selection = "single",
+            onClick = "select",
+            defaultPageSize = 15,
+            wrap = FALSE,
+            rowClass = JS("
             function(rowInfo) {
-            console.log(rowInfo);
-              if (rowInfo.selected) {
-                return 'selected-row';
+              console.log(rowInfo);
+                if (rowInfo.selected) {
+                  return 'selected-row';
+                }
               }
-            }
-          ")
-        ),
-        list(...)
+            ")
+          )
+          dataname_reactable
+          
+        }
       )
-      do.call(reactable::reactable, args = args)
     })
+    output$table <- reactable::renderReactable(table_q()[[dataname_reactable]])
+    table_selected_q <- reactive({
+      selected_row <- reactable::getReactableState("table", "selected")
+      if (!is.null(selected_row)) {
+        within(
+          table_q(),
+          selected_row = selected_row,
+          dataname_selected = str2lang(sprintf("%s_selected", dataname)),
+          dataname = str2lang(dataname),
+          expr = {
+            dataname_selected <- dataname[selected_row, ] 
+          }
+        )
+      } else {
+        table_q()
+      }
+    })
+    table_selected_q
   })
 }
