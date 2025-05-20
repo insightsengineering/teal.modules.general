78d35c97a9498947181b781f73ba6db84b38eb35
Dawid Kaledkowski <dawid.kaledkowski@gmail.com>
2025-05-20 11:58:46 +0200

    wip

--- R/tm_g_scatterplotmatrix.R
+++ R/tm_g_scatterplotmatrix.R
@@ -529,6 +529,7 @@ srv_g_scatterplotmatrix <- function(id,
       teal.reporter::simple_reporter_srv("simple_reporter", reporter = reporter, card_fun = card_fun)
     }
     ###
+    decorated_output_q
   })
 }
 
