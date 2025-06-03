61ab5541d487a0fb54b01e22bc969a9cc56d2fe3
Dawid Kaledkowski <dawid.kaledkowski@gmail.com>
2025-06-03 10:32:34 +0200

    replace grob with grid output

--- R/tm_g_association.R
+++ R/tm_g_association.R
@@ -347,7 +347,7 @@ srv_tm_g_association <- function(id,
     )
 
     qenv <- reactive(
-      teal.code::eval_code(data(), 'library("ggplot2");library("dplyr");library("tern");library("ggmosaic")') # nolint quotes
+      teal.code::eval_code(data(), 'library("ggplot2");library("dplyr");library("ggmosaic")') # nolint quotes
     )
     anl_merged_q <- reactive({
       req(anl_merged_input())
@@ -506,9 +506,7 @@ srv_tm_g_association <- function(id,
           substitute(
             expr = {
               plots <- plot_calls
-              plot_top <- plots[[1]]
-              plot_bottom <- plots[[2]]
-              plot <- tern::stack_grobs(grobs = lapply(list(plot_top, plot_bottom), ggplot2::ggplotGrob))
+              plot <- grid.arrange(plots[[1]], plots[[2]], ncol = 1)
             },
             env = list(
               plot_calls = do.call(
