74921a86aeb3678426f60c0566efacc18bbd1cf9
m7pr <marcin.kosinski.mk1@roche.com>
2025-09-08 13:54:34 +0200

    change function names in tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,4 +1,4 @@
-testthat::describe("modify_last_chunk_outputs_attributes", {
+testthat::describe("set_chunk_attrs", {
   card <- teal.reporter::teal_card(
     "# Header",
     "Some text",
@@ -7,26 +7,26 @@ testthat::describe("modify_last_chunk_outputs_attributes", {
   )
 
   it("changes last chunk output with default parameters", {
-    new_card <- modify_last_chunk_outputs_attributes(card, list(dev.height = 200))
+    new_card <- set_chunk_attrs(card, list(dev.height = 200))
     testthat::expect_equal(attributes(new_card[[4]]), list(class = "chunk_output", dev.height = 200))
     testthat::expect_equal(attributes(new_card[[3]]), list(class = "chunk_output"))
   })
 
   it("changes last 2 chunks", {
-    new_card <- modify_last_chunk_outputs_attributes(card, list(dev.height = 200), n = 2)
+    new_card <- set_chunk_attrs(card, list(dev.height = 200), n = 2)
     testthat::expect_equal(attributes(new_card[[4]]), list(class = "chunk_output", dev.height = 200))
     testthat::expect_equal(attributes(new_card[[3]]), list(class = "chunk_output", dev.height = 200))
   })
 
   it("only changes the numeric chunk_outputs", {
-    new_card <- modify_last_chunk_outputs_attributes(card, list(dev.height = 200), n = 2, inner_classes = "numeric")
+    new_card <- set_chunk_attrs(card, list(dev.height = 200), n = 2, inner_classes = "numeric")
     testthat::expect_equal(attributes(new_card[[3]]), list(class = "chunk_output", dev.height = 200))
     testthat::expect_equal(attributes(new_card[[4]]), list(class = "chunk_output"))
   })
 
   it("throws warning when last chunk is not chunk_output", {
     testthat::expect_warning(
-      modify_last_chunk_outputs_attributes(c(card, "yada"), list(new_attr = TRUE)),
+      set_chunk_attrs(c(card, "yada"), list(new_attr = TRUE)),
       "The last element of the `teal_card` is not a `chunk_output` object. No attributes were modified."
     )
   })
@@ -34,14 +34,14 @@ testthat::describe("modify_last_chunk_outputs_attributes", {
   it("throws warning when second to last chunk is not chunk_output", {
     card_modified <- c(card[c(1, 2, 3)], "bla", card[[4]])
     testthat::expect_warning(
-      modify_last_chunk_outputs_attributes(card_modified, n = 3, list(new_attr = TRUE)),
+      set_chunk_attrs(card_modified, n = 3, list(new_attr = TRUE)),
       "The 2 to last element of the `teal_card` is not a `chunk_output` object. Skipping any further modifications."
     )
   })
 
   it("modifies all elements up until the first non-chunk output", {
     card_modified <- c(card[c(1, 2, 3)], "bla", card[[4]])
-    new_card <- modify_last_chunk_outputs_attributes(card_modified, n = 3, list(new_attr = TRUE), quiet = TRUE)
+    new_card <- set_chunk_attrs(card_modified, n = 3, list(new_attr = TRUE), quiet = TRUE)
     testthat::expect_equal(attributes(new_card[[5]]), list(class = "chunk_output", new_attr = TRUE))
     testthat::expect_null(attributes(new_card[[4]]))
     testthat::expect_equal(attributes(new_card[[3]]), list(class = "chunk_output"))
