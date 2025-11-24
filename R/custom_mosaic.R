6fe6664c49bdf6293d3cbbb8f4ca7e1787c0ca85
André Veríssimo <211358+averissimo@users.noreply.github.com>
2025-11-24 10:24:29 +0000

    pr: feedback from @llrs-roche

--- R/custom_mosaic.R
+++ R/custom_mosaic.R
@@ -9,41 +9,35 @@
 #' @keywords internal
 .create_mosaic_layers <- function(data_name, x_var, y_var, reduce_plot_call) {
   data_call <- substitute(
-    mosaic_data <- data_name |>
+    mosaic_data <- data_name %>%
       # Count combinations of X and Y
-      dplyr::count(x_var, y_var) |>
+      dplyr::count(x_var, y_var) %>%
       # Compute total for each X group
-      dplyr::group_by(x_var) |>
       dplyr::mutate(
+        .by = x_var,
         x_total = sum(n),
         prop = n / x_total
-      ) |>
-      dplyr::ungroup() |>
+      ) %>%
       # Compute total sample size to turn counts into widths
-      dplyr::mutate(N_total = sum(x_total)) |>
+      dplyr::mutate(N_total = sum(x_total)) %>%
       # Convert counts to x widths
-      dplyr::group_by(x_var) |>
       dplyr::mutate(
-        x_width = x_total / unique(N_total)
-      ) |>
-      # Compute x-min/x-max for each group
-      dplyr::group_by(x_var) |>
-      dplyr::mutate(
+        .by = x_var,
+        x_width = x_total / unique(N_total),
         x_width_last = dplyr::if_else(dplyr::row_number() == dplyr::n(), x_width, 0)
-      ) |>
-      dplyr::ungroup() |>
+      ) %>%
+      # Compute x-min/x-max for each group
       dplyr::mutate(
         xmin = cumsum(dplyr::lag(x_width_last, default = 0)),
         xmax = xmin + x_width
-      ) |>
+      ) %>%
       # Compute y-min/y-max for stacked proportions
-      dplyr::group_by(x_var) |>
       dplyr::arrange(x_var, y_var) |>
       dplyr::mutate(
+        .by = x_var,
         ymin = c(0, head(cumsum(prop), -1)),
         ymax = cumsum(prop)
-      ) |>
-      dplyr::ungroup(),
+      ),
     env = list(x_var = as.name(x_var), y_var = as.name(y_var), data_name = as.name(data_name))
   )
 
@@ -60,13 +54,12 @@
 
   layer_scale_x <- substitute(
     ggplot2::scale_x_continuous(
-      breaks = mosaic_data |>
-        dplyr::distinct(x_var, xmin, xmax) |>
-        dplyr::rowwise() |>
-        dplyr::mutate(mid = (xmin + xmax) / 2) |>
+      breaks = mosaic_data %>%
+        dplyr::distinct(x_var, xmin, xmax) %>%
+        dplyr::mutate(mid = (xmin + xmax) / 2) %>%
         dplyr::pull(mid),
-      labels = mosaic_data |>
-        dplyr::distinct(x_var) |>
+      labels = mosaic_data %>%
+        dplyr::distinct(x_var) %>%
         dplyr::pull(x_var),
       expand = c(0, 0)
     ),
