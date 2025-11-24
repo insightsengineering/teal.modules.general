31957c579d9bd333735352b32ed1223cf879c810
Lluís Revilla Sancho <lluis.revilla_sancho@roche.com>
2025-11-24 10:03:45 +0100

    Remove mention to ggmosaic

--- R/teal.modules.general.R
+++ R/teal.modules.general.R
@@ -14,11 +14,5 @@
 #' @keywords internal
 "_PACKAGE"
 
-# nolint start.
-# Note ggmosaic (version <= 0.3.3) needs to be in DEPENDS as the following does not work if it is imported
-# df <- data.frame(x = c("A", "B", "C", "A"), y = c("Z", "Z", "W", "W"))
-# ggplot(df) + ggmosaic::geom_mosaic(aes(x = ggmosaic::product(x), fill = y))
-# nolint end.
-
 # Needed to avoid R CMD note on no visible binding
 utils::globalVariables("count")
