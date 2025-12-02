d1f46c9c7db7289528b37f31060104372236fc53
Lluís Revilla <185338939+llrs-roche@users.noreply.github.com>
2025-12-02 15:20:28 +0100

    Apply suggestions from code review
    Co-authored-by: André Veríssimo <211358+averissimo@users.noreply.github.com>
    Signed-off-by: Lluís Revilla <185338939+llrs-roche@users.noreply.github.com>

--- R/tm_missing_data.R
+++ R/tm_missing_data.R
@@ -1173,9 +1173,9 @@ srv_missing_data <- function(id,
         tile <- within(ANL_q,
           {
             by_variable_plot <- ggplot2::ggplot(ANL, ggplot2::aes(group_var_name, name)) +
-              ggplot2::geom_tile(ggplot2::aes(fill = column)) +
+              ggplot2::geom_tile(ggplot2::aes(fill = column), color = "gray90") +
               ggplot2::geom_text(ggplot2::aes(label = scales::percent(perc)),
-                data = . %>% dplyr::filter(perc > 0), color = "white"
+                data = ~ dplyr::filter(.x, perc > 0),
               ) +
               ggplot2::scale_x_discrete(expand = ggplot2::expansion()) +
               ggplot2::scale_fill_gradient(high = "#ff2951ff", low = "grey90", labels = labels) +
