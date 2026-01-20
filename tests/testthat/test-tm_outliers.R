02c711d591049776b9822ffdc5c9b5bfffa28002
osenan <oriol@praenoscere.com>
2026-01-20 16:47:38 +0100

    feat: add more test for input validation and module creation

--- tests/testthat/test-tm_outliers.R
+++ tests/testthat/test-tm_outliers.R
@@ -142,4 +142,53 @@ testthat::describe("tm_outliers module creation", {
       "teal_module"
     )
   })
+
+  it("creates an ui of the expected type", {
+    mod <- tm_outliers(outlier_var = outlier_var)
+    testthat::expect_s3_class(
+      mod$ui("test", outlier_var = outlier_var),
+      "shiny.tag.list"
+    )
+  })
+})
+
+testthat::describe("test for input validation", {
+  data <- teal_data()
+  data <- within(data, {
+    CO2 <- CO2
+    CO2[["primary_key"]] <- seq_len(nrow(CO2))
+  })
+  join_keys(data) <- join_keys(join_key("CO2", "CO2", "primary_key"))
+  vars <- choices_selected(variable_choices(data[["CO2"]], c("Plant", "Type", "Treatment")))
+  outlier_var <- list(
+          var1 = data_extract_spec(
+              dataname = "CO2",
+              select = select_spec(
+                label = "Select variable:",
+                choices = variable_choices(data[["CO2"]], c("conc", "uptake")),
+                selected = "uptake",
+                multiple = FALSE,
+                fixed = FALSE
+              )
+            )
+        )
+
+  it("fails if no outlier_var defined", {
+    testthat::expect_error(tm_outliers(), "argument \"outlier_var\" is missing, with no default")
+  })
+
+  it("fails if outlier_var is not of the expected type", {
+    testthat::expect_error(tm_outliers(outlier_var = list(wrong_type = "wrong_type")),
+    "Assertion on 'outlier_var' failed")
+  })
+
+  it("fails if categorical_var is not of the expected type", {
+    testthat::expect_error(tm_outliers(outlier_var = outlier_var, categorical_var = list(wrong_type = "wrong_type")),
+    "Assertion on 'categorical_var' failed")
+  })
+
+  it("fails if ggtheme is not within expected values", {
+    testthat::expect_error(tm_outliers(outlier_var = outlier_var, ggtheme = "wrong_type"),
+    "'arg' should be one of")
+  })
 })
