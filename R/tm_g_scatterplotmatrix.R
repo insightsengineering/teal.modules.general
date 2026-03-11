48818368c76ae13b155406aaf68c50fe20cd3f62
m7pr <marcin.kosinski.mk1@roche.com>
2026-03-11 14:39:24 +0100

    change patchwork to ggplot object

--- R/tm_g_scatterplotmatrix.R
+++ R/tm_g_scatterplotmatrix.R
@@ -26,7 +26,7 @@
 #' @section Decorating Module:
 #'
 #' This module generates the following objects, which can be modified in place using decorators:
-#' - `plot` (`patchwork` - assembled from individual `ggplot` panels)
+#' - `plot` (`ggplot` - assembled from individual `ggplot` panels)
 #'
 #' A Decorator is applied to the specific output using a named list of `teal_transform_module` objects.
 #' The name of this list corresponds to the name of the output to which the decorator is applied.
